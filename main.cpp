2f0ace50e92a9c379655636d544b8db142ddd710
clara-samarino <anasamarino123456@hotmail.com>
2022-01-22 14:48:32 -0300

    removendo variais e linhas inuteis do main

--- main.cpp
+++ main.cpp
@@ -127,9 +127,6 @@ int main()
     comissao03 += e03.comissao(v06.VALOR);
     numAtendimentos03+=1;
 
-    v06.descricao = "Decoracao de Apartamento no Brooklyn";
-    v06.VALOR = 3000;
-
     Venda v07;
     v07.cliente= "J. Jonah Jameson";
     v07.esp = e03;
