b6d06d4a49bcadadd5e88804fa573ddbbabc323e
Vinicius Alves <viniciusfariaresende@gmail.com>
2022-01-22 10:22:30 -0300

    fix: removing unused attribute from Cliente class

--- include/Cliente.hpp
+++ include/Cliente.hpp
@@ -9,7 +9,6 @@ class Cliente{
 		std::string nome;
 		std::string endereco;
 		std::string cep;
-		std::string alturaDosPais;
 
 		void print(); // imprime na tela os dados de um cliente cadastrado
 
