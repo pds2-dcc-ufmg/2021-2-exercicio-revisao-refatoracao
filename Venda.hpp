4d3ae00ad5bdba59e3733b6aca9b08a6e671ae5e
Vinicius Ferreira <vinicius01cerqueira@hotmail.com>
2022-01-25 20:57:14 -0300

    formatado

--- Venda.hpp
+++ Venda.hpp
@@ -6,24 +6,21 @@
 #include "Cliente.hpp"
 #include "Especialista.hpp"
 
-
 class Venda{
-
     public:
+
     double Valor;
-    std::string descricao;
-    Especialista esp;
-    std::string cliente;
+        std::string descricao;
+        Especialista esp;
+        std::string cliente;
 
+        void print() {
+            std::cout << "Especialista: "<< std::endl;
+            std::cout << esp.Nome << std::endl;
 
-    void print() {
-
-        std::cout << "Especialista: "<< std::endl;
-        std::cout << esp.Nome << std::endl;
-
-        std::cout << " Cliente: "<< std::endl;
-        std::cout << cliente<< std::endl;
-   }
+            std::cout << " Cliente: "<< std::endl;
+            std::cout << cliente<< std::endl;
+       }
 };
 
 #endif
