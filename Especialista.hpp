4bf3cf8d22074dd5ffbef18d225e5efd5845854e
Vinicius Ferreira <vinicius01cerqueira@hotmail.com>
2022-01-25 20:24:39 -0300

    nova formatação e alterações.

--- Especialista.hpp
+++ Especialista.hpp
@@ -22,7 +22,7 @@ class Especialista : public Funcionario {
 
         void print() {
             std::cout << "[Especialista]" << std::endl
-	    << "Especialidade: " << especialidade <<std::endl;
+	    << " Especialidade: " << especialidade <<std::endl;
             Funcionario::print();
             std::cout << "  Nome: " << nome << std::endl
             << "  SalarioBase: R$ " << fixed << setprecision(2) << SalarioBase << std::endl;
