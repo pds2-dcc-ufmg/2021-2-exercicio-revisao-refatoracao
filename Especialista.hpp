00dfda6214c7baec1679b64edb427781a12fb5a0
Vinicius Ferreira <vinicius01cerqueira@hotmail.com>
2022-01-25 19:45:37 -0300

    nova formatação e alteraçãoes

--- Especialista.hpp
+++ Especialista.hpp
@@ -4,36 +4,29 @@
 #include "Funcionario.hpp"
 #include "Cliente.hpp"
 
-using namespace std;
-
-double perc = 0.1;
-double percWanda = 0.1;
-
 
 class Especialista : public Funcionario {
 
-    public:
+    private:
+
+      double perc = 0.1;
 
     public:
-        string especialidade;
 
-    double comissao(double ValorVenda) {
-        double c = ValorVenda*perc;
-                  return c;
-    }
+        std::string especialidade;
 
+        double comissao(double ValorVenda) {
+            double c = ValorVenda*perc;
+                return c;
+        }
 
-    void print() {
-
-        std::cout << "[Especialista]" << endl;
-        Funcionario::print();
-
-
-
-        std::cout << "  Nome: " << nome << endl
-        << "  SalarioBase: R$ " << fixed << setprecision(2) << SalarioBase <<endl;
-
-    }
+        void print() {
+            std::cout << "[Especialista]" << std::endl
+	    << "Especialidade: " << especialidade <<std::endl;
+            Funcionario::print();
+            std::cout << "  Nome: " << nome << std::endl
+            << "  SalarioBase: R$ " << fixed << setprecision(2) << SalarioBase << std::endl;
+        }
 };
 
 #endif
