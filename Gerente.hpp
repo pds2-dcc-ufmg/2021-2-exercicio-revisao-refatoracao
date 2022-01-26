4698d305725a70104e31550431de4135ac6cabcd
rafakesia <98476709+rafakesia@users.noreply.github.com>
2022-01-26 20:24:53 -0300

    Update Gerente.hpp
    - Alteração da classe Gerente para uma classe do tipo Funcionário;
    - Consequentemente, foi feita a retirada das variáveis nome, idade, etc, que serão agora acessadas a partir da classe Funcionário;
    - Padronização da declaração de variáveis;
    - Padronização da identação;
    - Retirada de quebras de linha desnecessárias;
    - Retirada da variável x na função que calcula a bonificação, pois ela não é utilizada.

--- Gerente.hpp
+++ Gerente.hpp
@@ -6,32 +6,18 @@
 
 using namespace std;
 
-double ValorBONIFICACAO = 15.0;
+double valorBonificacao = 15.0;
 
-class Gerente {
-    public:
-        double SalarioBase; // valor mínimo recebido pelo funcionário
-        string IDADE;
-        string nome;
-        int rgFunc;
-        double bonificacao;
+class Gerente:public Funcionario{
+  public:
+    void print() {
+      cout << "[Funcionario]" << endl << "[Gerente]" << endl << "Nome: " << nome << endl << "Idade: " << idade << endl
+      << "RGFunc: " << rgFunc << endl << "SalarioBase: R$ " << fixed << setprecision(2) << salarioBase <<endl;
+    }
 
-
-        void print() {
-            cout << "[Funcionario]" << endl
-            << "[Gerente]" << endl
-            << "  Nome: " << nome << endl
-            << "  Idade: " << IDADE << endl
-            << "  RGFunc: " << rgFunc << endl
-            << "  SalarioBase: R$ " << fixed << setprecision(2) << SalarioBase <<endl;
-        }
-
-        double calcula_BONIFICACAO_GERENTE(int numTOTALVendas){
-            double x;
-            return numTOTALVendas*ValorBONIFICACAO;
-        }
-
+    double calculaBonificacaoGerente(int numTotalVendas){
+      return numTotalVendas*valorBonificacao;
+    }
 };
 
 #endif
-
