acfb081e7d8c6995d59810c7cde3fc79e409d8e8
arthurvdeassis <98416842+arthurvdeassis@users.noreply.github.com>
2022-01-26 07:30:59 -0300

    Update Cliente.cpp

--- Cliente.cpp
+++ Cliente.cpp
@@ -2,10 +2,10 @@
 #include <string>
 #include "Cliente.hpp"
 
-void Cliente::print(){
+void Cliente::print() {
 
 	std::cout << "[Cliente]" << std::endl;
-        std::cout << "  Nome: " << NOME << std::endl;
+        std::cout << "  Nome: " << nome << std::endl;
 	std::cout << "  Endereco: " << endereco << std::endl;
-	std::cout << "  CEP: " << Cep << std::endl;
+	std::cout << "  CEP: " << cep << std::endl;
 }
