c0ebad080320337ea964674cf0378d9cb139bb29
GustavoTCorrea <94574419+GustavoTCorrea@users.noreply.github.com>
2022-01-27 17:47:26 -0300

    Adição de um construtor para clientes

--- Cliente.hpp
+++ Cliente.hpp
@@ -7,6 +7,12 @@ using namespace std;
 
 class Cliente{
 	public:
+        //Construtor de cliente
+        Cliente(string NOME = "",
+            string endereco = "",
+            string Cep = "",
+            string AlturaDosPais = "");
+
 		string NOME;
 		string endereco;
 		string Cep;
