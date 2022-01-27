845024ccd88807cee2ca011790457cb04d83086e
GustavoTCorrea <94574419+GustavoTCorrea@users.noreply.github.com>
2022-01-27 17:00:46 -0300

    Alteração para que a saída do main não mude
    Não havia percebido antes que a mudança feita alteraria a saída, corrigi isso.

--- Cliente.cpp
+++ Cliente.cpp
@@ -4,9 +4,8 @@
 
 //Imprime as informações do cliente, na ordem nome, endereço e CEP.
 void Cliente::print(){
-	cout << "[Cliente]: " << endl; 
+	cout << "[Cliente]" << endl; 
 	cout << "  Nome: " << NOME << endl;
 	cout << "  Endereco: " << endereco << endl;
 	cout << "  CEP: " << Cep << endl;
 }
-
