ca144bf307a92312b3be09a94b0a915e3e6cb795
rafakesia <98476709+rafakesia@users.noreply.github.com>
2022-01-27 12:56:58 -0300

    Final update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-
+#include "Cliente.cpp"
 #include "Cliente.hpp"
 #include "Funcionario.hpp"
 #include "Especialista.hpp"
@@ -11,31 +11,31 @@ int main(){
   Cliente cliente1;
   cliente1.nome = "J. Jonah Jameson";
   cliente1.endereco = "Nova York";
-  cliente1.cep = "35690000";
+  cliente1.cep = 35690000;
   cliente1.print();
 
   Cliente cliente2;
   cliente2.nome = "Norman Osborn";
   cliente2.endereco = "Hartlford";
-  cliente2.cep = "22061955";
+  cliente2.cep = 22061955;
   cliente2.print();
 
   Cliente cliente3;
   cliente3.nome = "Otto Octavius";
   cliente3.endereco = "Schenectady";
-  cliente3.cep = "24051953";
+  cliente3.cep = 24051953;
   cliente3.print();
 
   Cliente cliente4;
   cliente4.nome = "Bruce Benner";
   cliente4.endereco = "Dayton";
-  cliente4.cep = "22111967";
+  cliente4.cep = 22111967;
   cliente4.print();
 
   Cliente cliente5;
   cliente5.nome = "Steve Rogers";
   cliente5.endereco = "Lower East Side";
-  cliente5.cep = "13061981";
+  cliente5.cep = 13061981;
   cliente5.print();
 
   Especialista e01;
@@ -43,7 +43,7 @@ int main(){
   double comissao01 = 0;
   e01.nome = "Peter Parker";
   e01.idade = "46";
-  e01.rgFunc = 27061975;
+  e01.rgNumber = 27061975;
   e01.salarioBase = 3000;
   e01.especialidade = "Fotografia";
 
@@ -52,7 +52,7 @@ int main(){
   double comissao02 = 0;
   e02.nome = "Tony Stark";
   e02.idade = "56";
-  e02.rgFunc = 4041965;
+  e02.rgNumber = 4041965;
   e02.salarioBase = 1000;
   e02.especialidade = "Consertos de equipamentos eletronicos";
 
@@ -61,7 +61,7 @@ int main(){
   double comissao03 = 0;
   e03.nome = "Wanda Maximoff";
   e03.idade = "32";
-  e03.rgFunc = 16021989;
+  e03.rgNumber = 16021989;
   e03.salarioBase = 5000;
   e03.especialidade = "Engenharia e Designeeeeer";
 
@@ -69,7 +69,7 @@ int main(){
   double bonificacao01 = 0;
   g01.nome = "Nick Fury";
   g01.idade = "72";
-  g01.rgFunc = 21121948;
+  g01.rgNumber = 21121948;
   g01.salarioBase = 10000;
 
   Venda v01;
@@ -133,48 +133,48 @@ int main(){
   v08.cliente = "Otto Octavius";
   v08.esp = e02;
   v08.descricao = "Formatacao do PC";
-  v08.VALOR = 80;
+  v08.valor = 80;
   comissao02 += e02.comissao(v08.valor);
   numAtendimentos02+=1;
 
   cout << "Relatorio das Vendas" << endl;
 
   v01.print();
-  cout << "Descricao: " << v01.descricao << endl;
+  cout << "\nDescricao: " << v01.descricao << endl << endl;
 
   v02.print();
-  cout << "Descricao: " << v02.descricao << endl;
+  cout << "\nDescricao: " << v02.descricao << endl << endl;
 
   v03.print();
-  cout << "Descricao: " << v03.descricao << endl;
+  cout << "\nDescricao: " << v03.descricao << endl << endl;
 
   v04.print();
-  cout << "Descricao: " << v04.descricao << endl;
+  cout << "\nDescricao: " << v04.descricao << endl << endl;
 
   v05.print();
-  cout << "Descricao: " << v05.descricao << endl;
+  cout << "\nDescricao: " << v05.descricao << endl << endl;
 
   v06.print();
-  cout << "Descricao: " << v06.descricao << endl;
+  cout << "\nDescricao: " << v06.descricao << endl << endl;
 
   v07.print();
-  cout << "Descricao: " << v07.descricao << endl;
+  cout << "\nDescricao: " << v07.descricao << endl << endl;
 
   v08.print();
-  cout << "Descricao: " << v08.descricao << endl;
+  cout << "\nDescricao: " << v08.descricao << endl << endl;
 
-  cout << "Relatorio dos Funcionarios" << endl;
+  cout << "Relatorio dos Funcionarios:" << endl;
   e01.print();
   cout << "Num Atendimentos: " << numAtendimentos01 << endl;
-  cout << "Salario Total: " << e01.salarioBase + comissao01 << endl;
+  cout << "Salario Total: " << e01.salarioBase + comissao01 << endl << endl;
 
   e02.print();
   cout << "Num Atendimentos: " << numAtendimentos02 << endl;
-  cout << "Salario Total: " << e02.salarioBase + comissao02 << endl;
+  cout << "Salario Total: " << e02.salarioBase + comissao02 << endl << endl;
 
   e03.print();
   cout << "Num Atendimentos: " << numAtendimentos03 << endl;
-  cout << "Salario Total: " << e03.salarioBase + comissao03 << endl;
+  cout << "Salario Total: " << e03.salarioBase + comissao03 << endl << endl;
 
   int numTotalVendas = 0;
   numTotalVendas = numAtendimentos01 + numAtendimentos02 + numAtendimentos03;
