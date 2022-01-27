0a4db35a699aaaea8b76129d275749b41503c0d3
004-4 <80779444+004-4@users.noreply.github.com>
2022-01-27 18:29:17 -0300

    Create Funcionario.cpp

---
+++ Funcionario.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+#include "Funcionario.hpp"
+
+void Funcionario:: print() {
+
+    cout << "Funcionario:" << endl
+         << "  Idade: " << idade << endl
+         << "  RGFunc: " << rgFunc << endl;
+
+}
