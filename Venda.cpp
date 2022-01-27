bd22b50e6dfdf21955697514ca0b7787922ee774
004-4 <80779444+004-4@users.noreply.github.com>
2022-01-27 19:14:54 -0300

    Create Venda.cpp

---
+++ Venda.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+#include "Venda.hpp"
+
+void Venda:: print() {
+        
+    cout << "Especialista: "
+         << esp.nome << "; "
+         << " Cliente: "
+         << cliente << "; ";
+
+}
