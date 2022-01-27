d6c542975e065d4d79a12e28005806ba1ad01f0b
004-4 <80779444+004-4@users.noreply.github.com>
2022-01-27 18:09:30 -0300

    Update Cliente.cpp
    A identação foi ajustada, o comando de impressão foi mudado de std::cout para cout, pois o namespace std está incluido em Cliente.hpp, a impressão foi alterada de [Cliente] para Cliente: , e as variáveis tiveram seus nomes alterados de acordo com as mudanças feitas em Cliente.hpp

--- Cliente.cpp
+++ Cliente.cpp
@@ -2,12 +2,13 @@
 #include <string>
 #include "Cliente.hpp"
 
-void Cliente::print(){
+void Cliente::print() {
 
-	std::cout << "[Cliente]" << endl
-         << "  Nome: " << NOME << endl
+	cout << "Cliente:" << endl
+         << "  Nome: " << nome << endl
 		 << "  Endereco: " << endereco << endl
-		 << "  CEP: " << Cep << endl;
+		 << "  CEP: " << cep << endl << endl;
 
 }
+
 
