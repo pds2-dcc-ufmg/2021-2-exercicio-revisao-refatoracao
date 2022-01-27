112f369ab650350877392f0e140daaa61c32357e
GustavoTCorrea <94574419+GustavoTCorrea@users.noreply.github.com>
2022-01-27 17:46:01 -0300

    Adição de um construtor para cliente

--- Cliente.cpp
+++ Cliente.cpp
@@ -9,3 +9,13 @@ void Cliente::print(){
 	cout << "  Endereco: " << endereco << endl;
 	cout << "  CEP: " << Cep << endl;
 }
+
+Cliente::Cliente(string NOME,
+            string endereco,
+            string Cep,
+            string AlturaDosPais){
+  this->NOME = NOME;
+  this->endereco = endereco;
+  this->Cep = Cep;
+  this->AlturaDosPais = AlturaDosPais;
+}
