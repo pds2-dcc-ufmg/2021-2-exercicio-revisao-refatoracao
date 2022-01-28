c5dcbc08db820ba92e850c310ed2b5b971e3543c
quietbrelax-1313 <82960486+quietbrelax-1313@users.noreply.github.com>
2022-01-28 19:48:50 -0300

    Exercicio de Revisao

--- Funcionario.cpp
+++ Funcionario.cpp
@@ -6,70 +6,42 @@
 
 double Funcionario::GetSalarioBase()
 {
-<<<<<<< HEAD
-	return salarioBase;
-=======
     return salarioBase;
->>>>>>> e0fac57a8e834c8b58f46fce22d21d3e546bfbd8
 }
 
 std::string Funcionario::GetIdade()
 {
-<<<<<<< HEAD
 	return idade;
-=======
-    return idade;
->>>>>>> e0fac57a8e834c8b58f46fce22d21d3e546bfbd8
 }
 
 std::string Funcionario::GetNome()
 {
-<<<<<<< HEAD
 	return nome;
-=======
-    return nome;
->>>>>>> e0fac57a8e834c8b58f46fce22d21d3e546bfbd8
 }
 
 int Funcionario::GetRGFuncionario()
 {
-<<<<<<< HEAD
 	return rgFuncionario;
-=======
-    return rgFuncionario;
->>>>>>> e0fac57a8e834c8b58f46fce22d21d3e546bfbd8
 }
 
 void Funcionario::SetSalarioBase(double valorMinimo)
 {
-<<<<<<< HEAD
+
 	this -> salarioBase = valorMinimo;
-=======
-    this -> salarioBase = valorMinimo;
->>>>>>> e0fac57a8e834c8b58f46fce22d21d3e546bfbd8
 }
 
 void Funcionario::SetIdade(const std::string& _idade)
 {
-<<<<<<< HEAD
 	this -> idade = _idade;
-=======
-    this -> idade = _ idade;
->>>>>>> e0fac57a8e834c8b58f46fce22d21d3e546bfbd8
 }
 
 void Funcionario::SetNome(const std::string& _nome)
 {
-<<<<<<< HEAD
 	this -> nome = _nome;
-=======
-    this -> nome = _nome;
->>>>>>> e0fac57a8e834c8b58f46fce22d21d3e546bfbd8
 }
 
 void Funcionario::SetRGFuncionario(int RG)
 {
-<<<<<<< HEAD
 	this -> rgFuncionario = RG;
 }
 
@@ -80,32 +52,15 @@ void Funcionario::print()
 	<< " Idade: " << GetIdade() << std::endl
 	<< " RGFunc: " << GetRGFuncionario() << std::endl
 	<< " Salario Base: R$" << std::setprecision(2) << std::fixed << GetSalarioBase();
-=======
-    this -> rgFuncionario = RG;
 }
 
-void Funcionario::print()
-{
-    std::cout << "[Funcionario]" << std::endl
-    <<" Idade: " << GetIdade() << std::endl
-    <<" RGFunc: " << GetRGFuncionario() << std::endl;
->>>>>>> e0fac57a8e834c8b58f46fce22d21d3e546bfbd8
-}
 
 void Funcionario::print_oi()
 {
-<<<<<<< HEAD
 	std::cout << "Oi" << std::endl;
-=======
-    std::cout << "Oi" << std::endl;
->>>>>>> e0fac57a8e834c8b58f46fce22d21d3e546bfbd8
 }
 
 void Funcionario::print_tchau()
 {
-<<<<<<< HEAD
 	std::cout << "Tchau" << std::endl;
-=======
-    std::cout<< "Tchau" << std::endl;
->>>>>>> e0fac57a8e834c8b58f46fce22d21d3e546bfbd8
 }
