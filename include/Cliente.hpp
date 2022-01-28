3a3a13b2690e8f6fe74b60d59773d55857ab14c1
Vinicius Gomes <vinicius.svgomes@gmail.com>
2022-01-27 21:44:29 -0300

    Update Cliente.hpp

--- include/Cliente.hpp
+++ include/Cliente.hpp
@@ -7,15 +7,15 @@ class Cliente {
 	public:
 		Cliente(std::string nome, std::string endereco, std::string cep);
 	    
-        void imprime_dados();
+        	void imprime_dados();
 
-        void setNome(std::string nome);
-        void setEndereco(std::string endereco);
-        void setCep(std::string cep);
+        	void setNome(std::string nome);
+        	void setEndereco(std::string endereco);
+        	void setCep(std::string cep);
 
-        std::string getNome();
-        std::string getEndereco();
-        std::string getCep();
+        	std::string getNome();
+        	std::string getEndereco();
+        	std::string getCep();
 	
 	private:
 		std::string _nome, _endereco, _cep;
