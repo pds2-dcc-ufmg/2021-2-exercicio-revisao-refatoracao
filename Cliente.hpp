999ed6aedc057c99e74dfbf40fda015750b441c0
Carlos <caikleao@gmail.com>
2022-01-29 13:14:40 -0300

    Removendo espaços desnecessários do arquivo Cliente.hpp

--- Cliente.hpp
+++ Cliente.hpp
@@ -5,16 +5,13 @@
 using namespace std;
 
 class Cliente{
-
 	public:
-
 		string NOME;
 		string endereco;
 		string Cep;
 		string AlturaDosPais;
 
 		void print(); // imprime na tela os dados de um cliente cadastrado
-
 };
 
 #endif
