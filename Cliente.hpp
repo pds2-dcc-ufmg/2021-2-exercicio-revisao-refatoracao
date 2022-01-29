844a46eb7c613e1486495d07af1f695aedb5c3a2
Gus <gus.aquino42@gmail.com>
2022-01-29 10:19:24 -0300

    refatoracao 29.1.2022

--- Cliente.hpp
+++ Cliente.hpp
@@ -1,20 +1,28 @@
 #ifndef CLIENTE_H
 #define CLIENTE_H
 
+#include <iostream>
 #include <string>
-using namespace std;
+
 
 class Cliente{
 
 	public:
+		std::string NOME;	
+		std::string endereco;
+		std::string Cep;
+		std::string AlturaDosPais;
 
-		string NOME;
-		string endereco;
-		string Cep;
-		string AlturaDosPais;
-
-		void print(); // imprime na tela os dados de um cliente cadastrado
-
+		void print(); 
+		
 };
 
 #endif
+
+/*	*** Foi retirado o include namespace std e adicionado o std antes de cada uso 
+(O mesmo processo foi reproduzido no .cpp de implementação).
+	*** Ainda no .cpp foi retirado o "#include <string>", este include já é feito 
+no .hpp e paassado na chamada do "#include "Cliente.hpp"".
+	*** Por fim, foi acrescentado o ponteiro this-> na implementação ".cpp".
+					
+	*** Estas alterações serão aplicadas em qualquer outro código que não as sigam *** */
