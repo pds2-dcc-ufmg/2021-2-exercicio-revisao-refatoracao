58782c909aa4c901296b49ba861086106aa4be89
yanvictor <yanvictor@LAPTOP-HJQDN3N0.localdomain>
2022-01-30 18:01:31 -0300

    Criacao do arquivo Especialista.cpp

---
+++ Especialista.cpp
@@ -0,0 +1,22 @@
+#include "Especialista.hpp"
+#include "Funcionario.hpp"
+#include "Cliente.hpp"
+
+#include <iomanip>
+#include <iostream>
+
+double Especialista::comissao(double valorVenda) {
+	double valorComissao = valorVenda*percentualComissao;
+	return valorComissao;
+}
+
+
+void Especialista::print() {
+
+	std::cout << "[Especialista]" << std::endl;
+	Funcionario::print();
+
+        std::cout << "  Nome: " << nome << std::endl
+        	  << "  SalarioBase: R$ " << std::fixed << std::setprecision(2) << salarioBase << std::endl;
+
+}
