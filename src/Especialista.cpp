23bfb14134312829db6ea13d5a9cc40450f2a2f5
Mirna Mendonça <64574738+MirnaMendonca@users.noreply.github.com>
2022-01-29 22:14:19 -0300

    Update Especialista.cpp

--- src/Especialista.cpp
+++ src/Especialista.cpp
@@ -5,14 +5,14 @@
 
 Especialista::Especialista(std::string nome, int rgFuncionario, int idade, double salarioBase, std::string especialidade, double percentual, int numAtendimentos, double comissaoTotal) {
 
-    this->nome = nome;
-    this->rgFuncionario = rgFuncionario;
-    this->idade = idade;
-    this->salarioBase = salarioBase;
-    this->especialidade = especialidade;
-    this->percentual = percentual;
-    this->numAtendimentos = numAtendimentos;
-    this->comissaoTotal = comissaoTotal;
+	this->nome = nome;
+	this->rgFuncionario = rgFuncionario;
+	this->idade = idade;
+	this->salarioBase = salarioBase;
+	this->especialidade = especialidade;
+	this->percentual = percentual;
+	this->numAtendimentos = numAtendimentos;
+	this->comissaoTotal = comissaoTotal;
 
 }
 
@@ -48,22 +48,22 @@ int Especialista::getNumAtendimentos() {
 
 void Especialista::calcularComissao(double ValorVenda) {
 
-    this->comissaoTotal += ValorVenda*percentual;
-    this->numAtendimentos++;
+	this->comissaoTotal += ValorVenda*percentual;
+	this->numAtendimentos++;
 
 }
 
 void Especialista::print() const {
 
-    std::cout << "[Especialista]" << std::endl;
-    std::cout << "[Funcionario]" << std::endl
-            << "  Idade: " << this->idade << std::endl
-            << "  RGFunc: " << this->rgFuncionario << std::endl;
+	std::cout << "[Especialista]" << std::endl;
+	std::cout << "[Funcionario]" << std::endl
+	<< "  Idade: " << this->idade << std::endl
+	<< "  RGFunc: " << this->rgFuncionario << std::endl;
 
-    std::cout << "  Nome: " << this->nome << std::endl
-    << "  SalarioBase: R$ " << std::fixed << std::setprecision(2) << this->salarioBase << std::endl;
+	std::cout << "  Nome: " << this->nome << std::endl
+	<< "  SalarioBase: R$ " << std::fixed << std::setprecision(2) << this->salarioBase << std::endl;
 
-    std::cout << "Num Atendimentos: " << this->numAtendimentos << std::endl;
-    std::cout << "Salario Total: " << this->salarioBase + this->comissaoTotal << std::endl;
+	std::cout << "Num Atendimentos: " << this->numAtendimentos << std::endl;
+	std::cout << "Salario Total: " << this->salarioBase + this->comissaoTotal << std::endl;
 
 }
