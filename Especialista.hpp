aa261151c02494cfb2b68f69d867dbd727ffd7e0
JoaoJoaoVF <98504491+JoaoJoaoVF@users.noreply.github.com>
2022-01-30 16:08:47 -0300

    Atualização do código

--- Especialista.hpp
+++ Especialista.hpp
@@ -11,14 +11,14 @@ class Especialista : public Funcionario
 public:
     std::string especialidade; // especialidade do funcionario
 
-    double valor_Comissao(double ValorVenda) // valor da comissao a ser recebida pela venda feita
+    double valor_Comissao(double ValorVenda) // valor da comissao a ser recebida pela venda
     {
         return ValorVenda * PORCENTAGEM_COMISSAO;
     }
 
     void imprime_Especialista() //imprime os dados do especialista
     {
-        std::cout << "[Especialista]" << std::endl;
+        std::cout << "[Especialista] " << std::endl;
         Funcionario::imprime_Funcionario();
     }
 };
