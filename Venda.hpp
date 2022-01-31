762104250891f497f4b562ae575fcb9751e3af44
becaroline <88947044+becaroline@users.noreply.github.com>
2022-01-31 19:40:47 -0300

    Update Venda.hpp

--- Venda.hpp
+++ Venda.hpp
@@ -1,9 +1,9 @@
-
 #ifndef VENDA_HPP
 #define VENDA_HPP
 
 #include "Cliente.hpp"
 #include "Especialista.hpp"
+#include "Funcionario.hpp"
 
 #include<string>
 #include<iostream>
@@ -11,24 +11,28 @@
 using namespace std;
 
 class Venda{
-      
-      public:
-    
-      Venda(Cliente *cliente, Especialista *especialista, string descricao, double valor);
-    
-      void print();
-    
-      double _get_valor;
-    
-      private:
-      
-      string _descricao;
-    
-      Especialista* _especialista;
-    
-      Cliente* cliente;
-    
-      double valor;
+
+  public:
+    Venda(Cliente cli, Especialista* esp, string descricao, double valor) : cliente(cli), especialista(esp) {
+
+      this->descricao = descricao;
+      this->valor = valor;
+
+      esp->calcular_comissao(valor);
+      esp->num_atendimentos++;
+    }   
+
+    double valor;
+    string descricao;
+    Especialista* especialista;
+    Cliente cliente;
+
+    void print() {
+
+      cout << " Especialista: "  << especialista->nome
+           << " Cliente: "  << cliente.nome 
+           << " Descricao: "  << descricao << endl;
+    }
 };
 
 #endif
