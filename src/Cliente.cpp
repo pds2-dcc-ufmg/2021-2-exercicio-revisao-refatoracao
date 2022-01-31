5589276896f672ce2d4d0ab061e0246b7c6825e2
jorgesilva2407 <98708289+jorgesilva2407@users.noreply.github.com>
2022-01-31 15:27:55 -0300

    Adição do arquivo Cliente.cpp ao diretório src

---
+++ src/Cliente.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+#include "Cliente.hpp"
+
+void Cliente::print(){
+
+	std::cout << "[Cliente]" << endl
+         << "  Nome: " << NOME << endl
+		 << "  Endereco: " << endereco << endl
+		 << "  CEP: " << Cep << endl;
+
+}
+
