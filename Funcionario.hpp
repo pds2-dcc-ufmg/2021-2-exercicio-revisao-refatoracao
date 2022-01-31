26e8f23880fcf93e2c8f1a085c06a2f20f9abff8
cazuli <56639557+cazuli@users.noreply.github.com>
2022-01-30 22:39:27 -0300

    Update Funcionario.hpp
    Removida função print_oi pois ela não é necessária e aparenta ser uma função de teste

--- Funcionario.hpp
+++ Funcionario.hpp
@@ -8,7 +8,7 @@ using namespace std;
 
 class Funcionario {
     public:
-        double SalarioBase; // valor mÌnimo recebido pelo funcion·rio
+        double SalarioBase; // valor m√≠nimo recebido pelo funcion√°rio
         string IDADE;
         string nome;
         int rgFunc;
@@ -17,10 +17,6 @@ class Funcionario {
             cout << "[Funcionario]" << endl
             << "  Idade: " << IDADE << endl
             << "  RGFunc: " << rgFunc << endl;
-        }
-
-        void print_oi(){
-            cout << "Tchau" << endl;
         }
 };
 
