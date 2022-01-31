dd895daab5c23ecf133a69a5e97b0a8efd493704
Haroldo Neto <60106977+Dodooneto@users.noreply.github.com>
2022-01-31 17:26:04 -0300

    Update Cliente.hpp
    Adicionei comentários e organizei a estrutura do código

--- Cliente.hpp
+++ Cliente.hpp
@@ -5,18 +5,20 @@
 using namespace std;
 
 class Cliente{
-
-	public:
+    public:
+        /**
+        *@brief Imprime na tela os dados de um cliente cadastrado
+        */
+        void print();
 	
-		void print(); // imprime na tela os dados de um cliente cadastrado
-	
-	private:
-	
-		string NOME;
-		string endereco;
-		string Cep;
-		string AlturaDosPais;
-
+     private:
+	/**
+        *@brief Dados do cliente
+        */
+	string NOME;
+	string endereco;
+	string Cep;
+	string AlturaDosPais;
 };
 
 #endif
