c589c6bb2f776ad8bfd734e09fd62d75ee613269
Pe-Guedss <pedro.og2002@gmail.com>
2022-01-30 21:25:36 -0300

    Modificação do método print e do método comissao, que agora realiza retorno diretamente.

---
+++ Especialista.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+#include "Especialista.hpp"
+
+double Especialista::comissao (double ValorVenda) {
+
+    return ValorVenda * this->perc;
+}
+
+void Especialista::print() {
+
+    std::cout << "[Especialista]" << endl;
+    Funcionario::print();
+
+}
--- Especialista.hpp
+++ Especialista.hpp
@@ -4,36 +4,20 @@
 #include "Funcionario.hpp"
 #include "Cliente.hpp"
 
-using namespace std;
-
-double perc = 0.1;
-double percWanda = 0.1;
-
-
 class Especialista : public Funcionario {
 
     public:
 
-    public:
-        string especialidade;
+        virtual double comissao(double ValorVenda);
 
-    double comissao(double ValorVenda) {
-        double c = ValorVenda*perc;
-                  return c;
-    }
+        virtual void print();
 
 
-    void print() {
+    private:
 
-        std::cout << "[Especialista]" << endl;
-        Funcionario::print();
-
-
-
-        std::cout << "  Nome: " << nome << endl
-        << "  SalarioBase: R$ " << fixed << setprecision(2) << SalarioBase <<endl;
-
-    }
+        std::string especialidade;
+        
+        const double perc = 0.1;
 };
 
 #endif
