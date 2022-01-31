af6c803c975e7abe3a8ea2906d039ed97ee302ff
jorgesilva2407 <98708289+jorgesilva2407@users.noreply.github.com>
2022-01-31 18:27:08 -0300

    Update Cliente.hpp

--- include/Cliente.hpp
+++ include/Cliente.hpp
@@ -2,19 +2,36 @@
 #define CLIENTE_H
 
 #include <string>
-using namespace std;
 
-class Cliente{
+namespace Cliente{
 
-	public:
+	class Cliente{
 
-		string NOME;
-		string endereco;
-		string Cep;
-		string AlturaDosPais;
+		private:
 
-		void print(); // imprime na tela os dados de um cliente cadastrado
+			std::string __nome; 
+			std::string __endereco; 
+			std::string __Cep;
 
-};
+		public:
+
+			/* Retorna o nome do cliente */
+			std::string getNome();
+
+			/* Retorna o endereço do cliente */
+			std::string getEndereco();
+
+			/* Retorna o CEP do cliente */
+			std::string getCEP();
+
+			/* Método construtor da classe cliente */
+			Cliente(std::string nome, std::string endereco, std::string cep);
+
+			/* Imprime os dados de um determinado cliente */
+			void imprimeDados();
+
+	};
+
+}
 
 #endif
