e027380c2653faeb719e8bc850fd18d4b1c05fca
cazuli <56639557+cazuli@users.noreply.github.com>
2022-01-30 23:17:58 -0300

    Update Especialista.hpp
    removido extra public
    removido percwanda por fazer a mdsma coisa que perc
    renomeado perc para percentual para melhoria de clareza

--- Especialista.hpp
+++ Especialista.hpp
@@ -6,19 +6,16 @@
 
 using namespace std;
 
-double perc = 0.1;
-double percWanda = 0.1;
+double percentual = 0.1;
 
 
 class Especialista : public Funcionario {
-
-    public:
-
+    
     public:
         string especialidade;
 
     double comissao(double ValorVenda) {
-        double c = ValorVenda*perc;
+        double c = ValorVenda*percentual;
                   return c;
     }
 
