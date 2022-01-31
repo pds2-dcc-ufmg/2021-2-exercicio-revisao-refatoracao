23581a5fb1572ad2dc0bfbdaea20c5569d069216
becaroline <88947044+becaroline@users.noreply.github.com>
2022-01-31 19:37:07 -0300

    Update Especialista.hpp

--- Especialista.hpp
+++ Especialista.hpp
@@ -2,39 +2,48 @@
 #define Especialista_HPP
 
 #include "Funcionario.hpp"
-#include "Cliente.hpp"
 
 #include <string> 
+#include <iomanip>
 
 using namespace std;
 
 class Especialista : public Funcionario {
 
-    public:
+   public:
+    double comissao;
+    int num_atendimentos;
+    string especialidade;
+
+    Especialista(string nome, int idade, int rg, double salario_base, string especialidade){
+      this->rg = rg;
+      this->nome = nome;
+      this->idade = idade;
+      this->salario_base = salario_base;
+      this->especialidade = especialidade;
+
+      this->comissao = 0;
+      this->num_atendimentos = 0;
+    }
+
+    void calcular_comissao(double valor_venda) {
+      this->comissao += valor_venda * perc;
+    }
 
     void print() {
-        
-        Especialista(string nome, string idade, int rg_func, double salario_base, string especialidade);
-        
-        int get_num_atendimentos(); 
-        
-        void novo_atendimento();;
-        
-        void calcular_comissao(double valor_venda);
-        
-        void print() override;
+      cout << "[Especialista]" << endl
+          << "[Funcionario]" << endl
+          << "  Idade: " << idade << endl
+          << "  RGFunc: " << rg << endl
+          << "  Nome: " << nome << endl
+          << "  SalarioBase: R$ " 
+          << fixed << setprecision(2) << salario_base << endl
+          << "Num Atendimentos: " << num_atendimentos << endl
+          << "Salario Total: " << salario_base + comissao << endl;
     }
-    
-    private:
-    
-    string _especialidade;
-    
-    int _num_atendimentos = 0;
-    
-    double _comissao_acumulada = 0;
-    
-    double _percentual = 0.1;
-    
-};
 
+  private:
+    double perc = 0.1;
+
+};
 #endif
