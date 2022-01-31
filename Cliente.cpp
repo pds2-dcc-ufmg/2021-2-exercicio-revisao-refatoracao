61f0b00886fe7e2a08a3641884f2d9a0d86aabd7
fabriciolopss <fabriciolopss@ufmg.br>
2022-01-31 18:42:03 -0300

    Commit ajustando o Cliente.cpp e o Cliente.hpp

--- Cliente.cpp
+++ Cliente.cpp
@@ -5,14 +5,21 @@
 void Cliente::print(){
 
 	std::cout << "[Cliente]" << endl
-         << "  Nome: " << NOME << endl
-		 << "  Endereco: " << endereco << endl
-		 << "  CEP: " << Cep << endl;
+         << "  Nome: " << get_Nome << endl
+		 << "  Endereco: " << get_Cep << endl
+		 << "  CEP: " << get_Endereco << endl;
 
 }
-Cliente::Cliente (std::string Nome, std::string Endereco, std::string cep, std::string alturadospais){
-	this->NOME = Nome;
-	this->endereco = Endereco;
-	this->Cep = cep;
-	this->AlturaDosPais = alturadospais;
+
+std::string Cliente::get_Nome(){
+	return this->NOME;
 }
+
+std::string Cliente::get_Cep(){
+	return this->Cep;
+}
+
+std::string Cliente::get_Endereco(){
+	return this->endereco;
+}
+
--- Cliente.hpp
+++ Cliente.hpp
@@ -7,13 +7,11 @@ using namespace std;
 class Cliente{
 
 	public:
-
-
-
+		string get_Nome();
+		string get_Endereco();
+		string get_Cep();
+		
 		void print(); // imprime na tela os dados de um cliente cadastrado
-		Cliente(string nome, string Endereco, string cep, string alturadospais);
-
-	private:
 
 		string NOME;
 		string endereco;
