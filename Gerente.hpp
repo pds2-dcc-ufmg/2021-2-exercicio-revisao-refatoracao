acd82166b32f39fab77135b7c75d4e47993ec0b1
cazuli <56639557+cazuli@users.noreply.github.com>
2022-01-30 22:36:55 -0300

    Update Gerente.hpp
    Removida varaivel double x no método calcula_BONIFICACAO_GERENTE que não estava sendo utilizada

--- Gerente.hpp
+++ Gerente.hpp
@@ -10,7 +10,7 @@ double ValorBONIFICACAO = 15.0;
 
 class Gerente {
     public:
-        double SalarioBase; // valor mÌnimo recebido pelo funcion·rio
+        double SalarioBase; // valor m√≠nimo recebido pelo funcion√°rio
         string IDADE;
         string nome;
         int rgFunc;
@@ -27,7 +27,6 @@ class Gerente {
         }
 
         double calcula_BONIFICACAO_GERENTE(int numTOTALVendas){
-            double x;
             return numTOTALVendas*ValorBONIFICACAO;
         }
 
