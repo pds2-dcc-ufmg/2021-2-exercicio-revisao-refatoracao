4197cb95cc87aedde2ed14193b727331fb4ab4be
becaroline <88947044+becaroline@users.noreply.github.com>
2022-01-31 19:38:25 -0300

    Update Gerente.hpp

--- Gerente.hpp
+++ Gerente.hpp
@@ -3,29 +3,43 @@
 
 #include <iostream>
 #include <iomanip>
+#include <vector>
+#include <string>
 
-#include 'Funcionario.hpp'
+#include "Especialista.hpp"
 
 using namespace std;
 
-double ValorBONIFICACAO = 15.0;
+class Gerente : public Funcionario {
+  public:
+    double bonificacao = 15.0;
 
-class Gerente: public Funcionario{
-    public:
-        
-       Gerente(string nome, string idade, int rg_func, double salario_base);
-    
-       void print() override;
-    
-       void calcualar_bonificacao_gerente(num_total_vendas);
-    
-    private:
-    
-       double _bonificacao_acumulada = 0;
-    
-       double _valor_bonificacao = 15.0;
+    Gerente(string nome, int idade, int rg, double salario_base){
+      this->rg = rg;
+      this->nome = nome;
+      this->idade = idade;
+      this->salario_base = salario_base;
+    }
 
+    void print() {
+        cout << "[Funcionario]" << endl
+        << "[Gerente]" << endl
+        << "  Nome: " << nome << endl
+        << "  Idade: " << idade << endl
+        << "  RGFunc: " << rg << endl
+        << "  SalarioBase: R$ " << fixed << setprecision(2) << salario_base << endl
+        << "Salario Total: " << salario_base + bonificacao << endl;
+    }
+
+    void calcular_bonificacao(vector<Especialista*> especialistas){
+      int num_vendas = 0;
+
+      for(int i = 0; i < especialistas.size(); i++){
+        num_vendas += especialistas[i]->num_atendimentos;
+      }
+
+      this->bonificacao = num_vendas * bonificacao;
+    }
 };
 
 #endif
-
