524c167a1dcca81c6a491c41fa424cf848ab5345
becaroline <88947044+becaroline@users.noreply.github.com>
2022-01-31 15:57:07 -0300

    Update Cliente.hpp

--- Cliente.hpp
+++ Cliente.hpp
@@ -7,13 +7,18 @@ using namespace std;
 class Cliente{
 
 	public:
-
-		string NOME;
-		string endereco;
-		string Cep;
-		string AlturaDosPais;
-
+	
+		string altura_dos_pais;
+	
+                Cliente (string nome, string endereço, string cep)
+			
+		string get_nome();
+			
 		void print(); // imprime na tela os dados de um cliente cadastrado
+	
+       private:
+	 
+	        string _nome, _endereco, _cep;
 
 };
 
