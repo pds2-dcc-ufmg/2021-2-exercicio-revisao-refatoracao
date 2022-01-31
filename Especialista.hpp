d8bf668711999abdbb4c231e07b3782a5361a42d
Bruno-29 <98783290+Bruno-29@users.noreply.github.com>
2022-01-31 20:31:33 -0300

    Update Especialista.hpp

--- Especialista.hpp
+++ Especialista.hpp
@@ -6,32 +6,34 @@
 
 using namespace std;
 
-double perc = 0.1;
-double percWanda = 0.1;
+double PercentualDoGerente = 0.1;
+
 
 
 class Especialista : public Funcionario {
 
-    public:
+    
 
-    public:
+    private:
         string especialidade;
 
-    double comissao(double ValorVenda) {
-        double c = ValorVenda*perc;
-                  return c;
+    
+    public:
+    
+    double Comissao(double ValorVenda) {
+        return ValorVenda*PercentualDoGerente;            
     }
 
 
     void print() {
 
         std::cout << "[Especialista]" << endl;
+        
         Funcionario::print();
 
 
 
-        std::cout << "  Nome: " << nome << endl
-        << "  SalarioBase: R$ " << fixed << setprecision(2) << SalarioBase <<endl;
+        std::cout << "  Nome: " << Nome << endl << "  SalarioBase: R$ " << fixed << setprecision(2) << SalarioBase <<endl;
 
     }
 };
