7fa9ea56767eaa50e2c44ddf204dbf6144cbf7ff
Lucas Duarte <50371525+duartegit@users.noreply.github.com>
2022-02-01 20:51:46 -0300

    Update Cliente.cpp

--- Cliente.cpp
+++ Cliente.cpp
@@ -3,12 +3,10 @@
 #include "Cliente.hpp"
 
 void Cliente::print(){
-
 	std::cout << "[Cliente]" << endl
-         << "  Nome: " << nome << endl
-		 << "  Endereco: " << endereco << endl
-		 << "  CEP: " << cep << endl;
-
+        	  << "  Nome: " << nome << endl
+		  << "  Endereco: " << endereco << endl
+		  << "  CEP: " << cep << endl;
 }
 
 
