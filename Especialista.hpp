011e2e7be729d7cd0ad2c4b2d1df3dbaebc3922c
larisaguiar <86538713+larisaguiar@users.noreply.github.com>
2022-02-01 14:21:31 -0300

    Update Especialista.hpp

--- Especialista.hpp
+++ Especialista.hpp
@@ -4,36 +4,18 @@
 #include "Funcionario.hpp"
 #include "Cliente.hpp"
 
-using namespace std;
 
-double perc = 0.1;
 double percWanda = 0.1;
-
 
 class Especialista : public Funcionario {
 
     public:
 
     public:
-        string especialidade;
+    std::string especialidade;
 
-    double comissao(double ValorVenda) {
-        double c = ValorVenda*perc;
-                  return c;
-    }
-
-
-    void print() {
-
-        std::cout << "[Especialista]" << endl;
-        Funcionario::print();
-
-
-
-        std::cout << "  Nome: " << nome << endl
-        << "  SalarioBase: R$ " << fixed << setprecision(2) << SalarioBase <<endl;
-
-    }
-};
-
-#endif
+    double comissao(double ValorVenda);
+        
+    void print();
+}     
+ #endif
