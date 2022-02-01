45c58a3352a9fa2336b5bb8e34995c64adcd7b3e
Bruno-29 <98783290+Bruno-29@users.noreply.github.com>
2022-02-01 13:40:38 -0300

    Update Venda.hpp

--- Venda.hpp
+++ Venda.hpp
@@ -14,14 +14,36 @@ class Venda{
     
     double Valor;
     string Descricao;
-    Especialista Especialista;
+     Especialista *Especialista;
     string Cliente;
 
     public:
+
+
+    Venda(string cliente,string descricao,double valor, class Especialista &Especialista_ ){
+
+      this->Valor=valor;
+      this->Descricao=descricao;
+      this->Especialista= &Especialista_;
+      this->Cliente=cliente;
+
+
+    }
+
+       string getDescricao () {
+    return this->Descricao;
+  }
+
+    double getValor () {
+    return this->Valor;
+  }
+    
+     
+
     
     void print() {
 
-        std::cout << "Especialista: " << Especialista.Nome;
+        std::cout << "Especialista: " << Especialista->getNome();
         
 
         std::cout << " Cliente: " << Cliente;
