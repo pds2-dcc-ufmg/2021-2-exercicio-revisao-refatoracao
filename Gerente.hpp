452893928777bc12a26b41eab5872b87c3d4e13a
marcellohmp <marcellohenriquemp@gmail.com>
2022-02-01 19:08:35 -0300

    ultimo commit

--- Gerente.hpp
+++ Gerente.hpp
@@ -24,22 +24,22 @@ class Gerente : public Funcionario {
    * @param Nome Nome do Gerente envolvido.
    * @param RgFuncionario O rg do gerente envolvido.
    */
-        Gerente(double Salario, std::string Idade,
-         std::string Nome, int RgFuncionario);
+    Gerente(double Salario, std::string Idade,
+        std::string Nome, int RgFuncionario);
 
-        /**
-         * @brief Destrutor da classe
-         *
-         */
-        ~Gerente();
+    /**
+    * @brief Destrutor da classe
+    *
+    */
+    ~Gerente();
 
-		/**
-		 * @brief  Imprime na tela os dados de um Gerente cadastrado
-		 *
-		 */
-        void Print() const override;
+    /**
+    * @brief  Imprime na tela os dados de um Gerente cadastrado
+    *
+    */
+    void Print() const override;
 
-        double calculaBonificacaoGerente(int NumTotalVendas);
+    double calculaBonificacaoGerente(int NumTotalVendas);
 
 };
 
