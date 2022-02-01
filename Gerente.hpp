129ae70618a362acc4b75a3492b094904ce091bb
thvercosa <88744643+thvercosa@users.noreply.github.com>
2022-02-01 12:16:29 -0300

    Alterando Gerente.hpp

--- Gerente.hpp
+++ Gerente.hpp
@@ -4,33 +4,22 @@
 #include <iostream>
 #include <iomanip>
 
+#include <Funcionario.hpp>
+
 using namespace std;
 
-double ValorBONIFICACAO = 15.0;
+class Gerente : public Funcionario
+{
+private:
+    int numeroDeVendas;
 
-class Gerente {
-    public:
-        double SalarioBase; // valor mínimo recebido pelo funcionário
-        string IDADE;
-        string nome;
-        int rgFunc;
-        double bonificacao;
+public:
+    Gerente(double salarioBase, int idade, string nome, int rgFunc);
 
-
-        void print() {
-            cout << "[Funcionario]" << endl
-            << "[Gerente]" << endl
-            << "  Nome: " << nome << endl
-            << "  Idade: " << IDADE << endl
-            << "  RGFunc: " << rgFunc << endl
-            << "  SalarioBase: R$ " << fixed << setprecision(2) << SalarioBase <<endl;
-        }
-
-        double calcula_BONIFICACAO_GERENTE(int numTOTALVendas){
-            double x;
-            return numTOTALVendas*ValorBONIFICACAO;
-        }
-
+    void print();
+    double getBonificacao();
+    void addVendas(int);
+    double getSalarioTotal();
 };
 
 #endif
