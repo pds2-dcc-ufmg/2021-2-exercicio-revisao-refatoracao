b2d72779411bd041208d9c7ff3e62e30ba297a29
Bruno-29 <98783290+Bruno-29@users.noreply.github.com>
2022-02-01 13:39:26 -0300

    Update Funcionario.hpp

--- Funcionario.hpp
+++ Funcionario.hpp
@@ -7,7 +7,7 @@
 using namespace std;
 
 class Funcionario {
-    private:
+    protected:
     
     // @brief: valor mínimo recebido pelo funcionario
     
@@ -16,8 +16,24 @@ class Funcionario {
         string Nome;
         int RgDoFuncionario;
 
+        public:
+
+       Funcionario(string nome, string idade, int rgDoFuncionario, double salarioBase){
+
+         this->SalarioBase=salarioBase;
+      this->Idade=idade;
+      this->Nome=nome;
+      this->RgDoFuncionario=rgDoFuncionario;
+
+
+         
+       };
+
+        
+       
+
         void print() {
-            cout << "[Funcionario]" << endl  << "  Idade: " << Idade << endl  << "  RGFunc: " << RgDoFuncionario << endl;
+            cout << "[Funcionario]" << endl  << "  Idade: " << Idade << endl  << " RGFunc: " << RgDoFuncionario << endl;
         }
 
       
