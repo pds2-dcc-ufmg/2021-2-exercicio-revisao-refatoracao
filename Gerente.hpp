9e3dd42096251f28b35933d621f27c4f4044d7a0
Pedro-lucasf <98771738+Pedro-lucasf@users.noreply.github.com>
2022-02-01 16:07:24 -0300

    Update Gerente.hpp
    Retirei variável não utilizada, using namespace e comentários ruins, adicionei std::

--- Gerente.hpp
+++ Gerente.hpp
@@ -3,35 +3,30 @@
 
 #include <iostream>
 #include <iomanip>
+#include <string>
 
-using namespace std;
-
-double ValorBONIFICACAO = 15.0;
+double Valor_Bonificacao = 15.0;
 
 class Gerente {
     public:
-        double SalarioBase; // valor mínimo recebido pelo funcionário
-        string IDADE;
-        string nome;
+        double SalarioBase;
+        std::string IDADE;
+        std::string nome;
         int rgFunc;
         double bonificacao;
 
 
         void print() {
-            cout << "[Funcionario]" << endl
-            << "[Gerente]" << endl
-            << "  Nome: " << nome << endl
-            << "  Idade: " << IDADE << endl
-            << "  RGFunc: " << rgFunc << endl
-            << "  SalarioBase: R$ " << fixed << setprecision(2) << SalarioBase <<endl;
+            std::cout << "[Funcionario]" << std::endl
+            << "[Gerente]" << std::endl
+            << "  Nome: " << nome << std::endl
+            << "  Idade: " << IDADE << std::endl
+            << "  RGFunc: " << rgFunc << std::endl
+            << "  SalarioBase: R$ " << std::fixed << std::setprecision(2) << SalarioBase <<std::endl;
         }
 
-        double calcula_BONIFICACAO_GERENTE(int numTOTALVendas){
-            double x;
-            return numTOTALVendas*ValorBONIFICACAO;
+        double calcula_BONIFICACAO_GERENTE(int Total_Vendas){
+            return Total_Vendas*Valor_Bonificacao;
         }
-
 };
-
 #endif
-
