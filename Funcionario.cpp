aa7b17076fa684565e3b49c2ebbf1302798b946a
jao-07 <98784264+jao-07@users.noreply.github.com>
2022-02-01 18:57:02 -0300

    Update Funcionario.cpp

--- Funcionario.cpp
+++ Funcionario.cpp
@@ -1,1 +1,32 @@
+#include "Funcionario.hpp"
 
+Funcionario::Funcionario(double salarioBase, string idade, string nome, int RG){
+    
+  this->salarioBase = salarioBase;
+  this->idade = idade;
+  this->nome = nome;
+  this->RG = RG;
+}
+
+double Funcionario::get_salario() const{
+  return this->salarioBase;
+}
+
+string Funcionario::get_idade() const{
+  return this->idade;
+}
+
+string Funcionario::get_nome() const{
+  return this->nome;
+}
+
+int Funcionario::get_RG() const{
+  return this->RG;
+}
+
+void Funcionario::print() const{
+  cout << "[Funcionario]" << endl
+    << "  Idade: " << idade << endl
+    << "  RGFunc: " << RG << endl;
+    
+}
