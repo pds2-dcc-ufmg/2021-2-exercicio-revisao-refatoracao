a8cb347af842e1b45e55d12c2ce1ed162a132cf7
lucasbarroso1508 <98613709+lucasbarroso1508@users.noreply.github.com>
2022-01-31 21:55:59 -0300

    Update Cliente.hpp

--- Cliente.hpp
+++ Cliente.hpp
@@ -2,19 +2,16 @@
 #define CLIENTE_H
 
 #include <string>
+#include <iostream>
+
 using namespace std;
 
 class Cliente{
-
 	public:
-
-		string NOME;
-		string endereco;
-		string Cep;
-		string AlturaDosPais;
-
-		void print(); // imprime na tela os dados de um cliente cadastrado
-
+		Cliente(std::string nome, std::sring endereco, std::string cep);
+		void print();
+	private:
+		std::string _nome, std::string _endereco, std::string _cep;
 };
 
 #endif
