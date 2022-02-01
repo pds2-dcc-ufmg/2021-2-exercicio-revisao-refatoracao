d5fc633c0f81cbd778d5d8711c3d9855d05c6d41
Bruno-29 <98783290+Bruno-29@users.noreply.github.com>
2022-02-01 13:40:00 -0300

    Update Gerente.hpp

--- Gerente.hpp
+++ Gerente.hpp
@@ -8,19 +8,46 @@ using namespace std;
 
 double ValorBonificacao = 15.0;
 
-class Gerente {
+class Gerente : public Funcionario {
     private:
     
     // @brief: valor mínimo recebido pelo funcionario
+
         double SalarioBase; 
         string Idade;
         string Nome;
         int RgDoFuncionario;
         double Bonificacao;
 
+ public:
+
+         
+ Gerente(string nome, string idade, int rgDoFuncionario, double salarioBase): Funcionario( nome,  idade, rgDoFuncionario, salarioBase){
+      
+      this->SalarioBase=salarioBase;
+      this->Idade=idade;
+      this->Nome=nome;
+      this->RgDoFuncionario=rgDoFuncionario;
+
+      
+    }
+
+    double getSalarioBase () {
+    return this->SalarioBase;
+  }
+
+
+   double getBonificacao () {
+    return this->Bonificacao;
+  }
+      
+      void set_Bonificacao(int bonificacao) {
+         Bonificacao = bonificacao; 
+         }
+
 
         void print() {
-            cout << "[Funcionario]" << endl << "[Gerente]" << endl  << "  Nome: " << Nome << endl  << "  Idade: " << Idade << end << "  RGFunc: " << RgDoFuncionario << end  << "  SalarioBase: R$ " << fixed << setprecision(2) << SalarioBase <<endl;
+            cout << "[Funcionario]" << endl << "[Gerente]" << endl  << "  Nome: " << Nome << endl  << "  Idade: " << Idade << endl << "  RGFunc: " << RgDoFuncionario << endl  << "  SalarioBase: R$ " << fixed << setprecision(2) << SalarioBase <<endl;
         }
 
         double Calcula_Bonificacao_Gerente(int NumTotalVendas){
@@ -30,4 +57,5 @@ class Gerente {
 };
 
 #endif
+
 
