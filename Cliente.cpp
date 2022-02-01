51d85430907240bdfa8947cd8ec876a63d4e89a1
Lucas Duarte <50371525+duartegit@users.noreply.github.com>
2022-02-01 20:30:44 -0300

    Update Cliente.cpp

--- Cliente.cpp
+++ Cliente.cpp
@@ -13,34 +13,29 @@ void Cliente::print(){
 
 #/--get/set nome
 string Cliente::getNOME(){ 
-	
 	return NOME;
-
 }
-void Cliente::setNOME(string NOME_NOVO)
-{
+void Cliente::setNOME(string NOME_NOVO){
 	this->NOME = NOME_NOVO;
 }
 #---/
+
 
 #/--get/set endereço
 string Cliente::getENDERECO(){ 
 	return endereco;
 }
-
-void Cliente::setENDERECO(string ENDERECO_NOVO)
-{
+void Cliente::setENDERECO(string ENDERECO_NOVO){
 	this->endereco = ENDERECO_NOVO;
 }
 #---/
 
 
+#/--get/set cep
 string Cliente::getCEP(){ 
-	
 	return Cep;
 }
-
-void Cliente::setCEP(string CEP_NOVO)
-{
+void Cliente::setCEP(string CEP_NOVO){
 	this->Cep = CEP_NOVO;
 }
+#---/
