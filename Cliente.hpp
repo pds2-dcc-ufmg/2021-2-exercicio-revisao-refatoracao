8dfff71fa006b8fe8332d853c6cfaba7068c7a6e
Haroldo Neto <60106977+Dodooneto@users.noreply.github.com>
2022-01-31 22:02:29 -0300

    Update Cliente.hpp
    Adicionei a opção de definir os dados dos clientes

--- Cliente.hpp
+++ Cliente.hpp
@@ -10,15 +10,23 @@ class Cliente{
         *@brief Imprime na tela os dados de um cliente cadastrado
         */
         void print();
+        /**
+        *@brief Define os dados dos clientes
+	*@param nome Nome do cliente
+	*@param endereco Endereço do Cliente
+	*@param cep CEP do Cliente
+	*@param rg RG do Cliente
+        */	
+	void setDados(string nome, string endereco, string cep, string rg);
 	
      private:
 	/**
         *@brief Dados do cliente
         */
-	string NOME;
+	string nome;
 	string endereco;
-	string Cep;
-	string AlturaDosPais;
+	string cep;
+	string rg;
 };
 
 #endif
