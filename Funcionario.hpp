5bb6c544555628e75a41525386b14c5a8794aa24
ironsteve43 <70375780+ironsteve43@users.noreply.github.com>
2022-02-01 20:51:29 -0300

    Update Funcionario.hpp
    Definição das funções movidas para Funcionario.cpp

--- Funcionario.hpp
+++ Funcionario.hpp
@@ -8,20 +8,15 @@ using namespace std;
 
 class Funcionario {
     public:
-        double SalarioBase; // valor mÌnimo recebido pelo funcion·rio
+        double SalarioBase; // valor m√≠nimo recebido pelo funcion√°rio
         string IDADE;
         string nome;
         int rgFunc;
 
-        void print() {
-            cout << "[Funcionario]" << endl
-            << "  Idade: " << IDADE << endl
-            << "  RGFunc: " << rgFunc << endl;
-        }
+        
+        void print();
 
-        void print_oi(){
-            cout << "Tchau" << endl;
-        }
+        void print_oi();
 };
 
 #endif
