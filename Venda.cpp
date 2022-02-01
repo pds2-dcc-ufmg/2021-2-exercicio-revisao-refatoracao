a65d1d0bf99a3e732a1454533a593557b1cb86bb
Pedro-lucasf <98771738+Pedro-lucasf@users.noreply.github.com>
2022-02-01 18:23:27 -0300

    Create Venda.cpp
    Adicionei arquivo cpp com a implementação da função

---
+++ Venda.cpp
@@ -0,0 +1,10 @@
+#include "Venda.hpp"
+
+void Venda::print() {
+
+        std::cout << "Especialista: ";
+        std::cout << esp.nome;
+
+        std::cout << " Cliente: ";
+        std::cout << cliente;
+    }
