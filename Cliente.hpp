c1cd86b77adcd3925a520d082aeca8e4120bbd2f
Pedro-lucasf <98771738+Pedro-lucasf@users.noreply.github.com>
2022-02-01 15:51:58 -0300

    Update Cliente.hpp
    Retirei comentários desnecessários, e variável inutilizável

--- Cliente.hpp
+++ Cliente.hpp
@@ -1,19 +1,18 @@
 #ifndef CLIENTE_H
 #define CLIENTE_H
 
+#include <iostream>
 #include <string>
-using namespace std;
 
 class Cliente{
 
 	public:
 
-		string NOME;
-		string endereco;
-		string Cep;
-		string AlturaDosPais;
+		std::string NOME;
+		std::string endereco;
+		std::string Cep;
 
-		void print(); // imprime na tela os dados de um cliente cadastrado
+		void print();
 
 };
 
