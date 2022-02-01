de50bf2a50f242e3339f5f1763fd5b6b6533a37c
Marco Tristao <marcotuliopin@gmail.com>
2022-02-01 17:15:37 -0300

    Remove additional tag and move variables
    * Remove unused public tag.
    * Add new private tag.
    * Move variables perc and percWanda to inside private tag.

--- Especialista.hpp
+++ Especialista.hpp
@@ -6,34 +6,24 @@
 
 using namespace std;
 
-double perc = 0.1;
-double percWanda = 0.1;
-
-
 class Especialista : public Funcionario {
-
-    public:
-
     public:
         string especialidade;
 
-    double comissao(double ValorVenda) {
-        double c = ValorVenda*perc;
-                  return c;
-    }
+        double comissao(double ValorVenda) {
+            double c = ValorVenda*perc;
+                      return c;
+        }
 
-
-    void print() {
-
-        std::cout << "[Especialista]" << endl;
-        Funcionario::print();
-
-
-
-        std::cout << "  Nome: " << nome << endl
-        << "  SalarioBase: R$ " << fixed << setprecision(2) << SalarioBase <<endl;
-
-    }
+        void print() {
+            std::cout << "[Especialista]" << endl;
+            Funcionario::print(); 
+            std::cout << "  Nome: " << nome << endl
+            << "  SalarioBase: R$ " << fixed << setprecision(2) << SalarioBase <<endl;
+        }
+    private:
+        double perc = 0.1;
+        double percWanda = 0.1;
 };
 
 #endif
