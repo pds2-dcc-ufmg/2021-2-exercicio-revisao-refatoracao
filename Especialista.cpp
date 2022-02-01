19b9d2dc03d216970fd52dd1f88ca5a09f4dfb84
jao-07 <98784264+jao-07@users.noreply.github.com>
2022-02-01 19:37:46 -0300

    Update Especialista.cpp

--- Especialista.cpp
+++ Especialista.cpp
@@ -1,1 +1,23 @@
 #include "Especialista.hpp"
+
+Especialista::Especialista(double salarioBase, string idade, string nome, int RG, string especialidade){
+  this->salarioBase = salarioBase;
+  this->idade = idade;
+  this->nome = nome;
+  this->RG = RG;
+  this->especialidade = especialidade;
+}
+
+double Especialista::comissao(double ValorVenda){
+  return PERCENTUAL * ValorVenda;
+}
+
+void Funcionario::print() const{
+  
+  cout << "[Especialista]" << endl
+   << "[Funcionario]" << endl
+   << "  Idade: " << idade << endl
+   << "  RGFunc: " << RG << endl
+   << "  Nome: " << nome << endl
+   << "  SalarioBase: R$ " << fixed << setprecision(2) << salarioBase << endl;
+}
