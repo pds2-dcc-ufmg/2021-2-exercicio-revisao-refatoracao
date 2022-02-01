3b26b7067796c20f9485bee2da4da630dac72a4a
jao-07 <98784264+jao-07@users.noreply.github.com>
2022-02-01 18:45:40 -0300

    Update Funcionario.hpp

--- Funcionario.hpp
+++ Funcionario.hpp
@@ -10,11 +10,11 @@ class Funcionario : public Pessoa {
     public:
     
         Funcionario(double salarioBase, string idade, string nome, int RG);
-        void print() const override;
         double get_salario() const;
         string get_idade() const;
         string get_nome() const;
         int get_RG() const;
+        void print() const override;
     
     private:
     
