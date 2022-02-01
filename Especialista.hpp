7da10a6df06ddec5c86604f930f95d99143f4947
Iago Rios <63796833+iago-r@users.noreply.github.com>
2022-02-01 17:41:59 -0300

    Update Especialista.hpp

--- Especialista.hpp
+++ Especialista.hpp
@@ -1,39 +1,28 @@
 #ifndef Especialista_HPP
 #define Especialista_HPP
 
+double const PORCENTAGEM = 0.1;
+
 #include "Funcionario.hpp"
-#include "Cliente.hpp"
 
-using namespace std;
-
-double perc = 0.1;
-double percWanda = 0.1;
-
+//int totalDeServicos = 0;
 
 class Especialista : public Funcionario {
+    public:
+        Especialista();
+        Especialista(std::string _nome,
+                    std::string _idade,
+                    int _rgFunc,
+                    double _salarioBase,
+                    std::string _especialidade);
 
     public:
+        std::string especialidade;
+        int numDeAtendimentos = 0;
+        double comissao = 0;
 
-    public:
-        string especialidade;
-
-    double comissao(double ValorVenda) {
-        double c = ValorVenda*perc;
-                  return c;
-    }
-
-
-    void print() {
-
-        std::cout << "[Especialista]" << endl;
-        Funcionario::print();
-
-
-
-        std::cout << "  Nome: " << nome << endl
-        << "  SalarioBase: R$ " << fixed << setprecision(2) << SalarioBase <<endl;
-
-    }
+        void adicionaComissao(double valorVenda);
+        void print();
 };
 
 #endif
