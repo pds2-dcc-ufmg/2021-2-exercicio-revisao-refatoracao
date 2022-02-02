d1f3775727519bb4d62379426a94fd0fc9ae7bbd
Pedro-lucasf <98771738+Pedro-lucasf@users.noreply.github.com>
2022-02-01 21:33:19 -0300

    Update Venda.hpp
    Coloquei as variaveis no private, e adicionei metodos set e get para cada variavel.

--- Venda.hpp
+++ Venda.hpp
@@ -8,13 +8,26 @@
 class Venda{
 
     public:
+    void print();
+    
+    void set_VALOR(double _VALOR);
+	double get_VALOR();
+		
+	void set_descricao(std::string _descricao);
+	std::string get_descricao();
+	
+	void set_esp(Especialista _esp);
+	Especialista get_esp();
+		
+	void set_cliente(std::string _cliente);
+	std::string get_cliente();
+    
+    private:
+    
     double VALOR;
     std::string descricao;
     Especialista esp;
     std::string cliente;
-
-
-    void print();
 };
 
 #endif
