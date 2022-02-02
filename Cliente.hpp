71f2b53f57e89796617ecfdf7544d7911bdeeb14
Kurotaki-G <81437746+Kurotaki-G@users.noreply.github.com>
2022-02-01 22:55:46 -0300

    Set Indentation
    ajuste de indentação e espaçamento

--- Cliente.hpp
+++ Cliente.hpp
@@ -1,19 +1,16 @@
 #ifndef CLIENTE_H
 #define CLIENTE_H
-
 #include <string>
 using namespace std;
 
 class Cliente{
 
-	public:
-
-		string NOME;
-		string endereco;
-		string Cep;
-
-		void print(); // imprime na tela os dados de um cliente cadastrado
-
+    public:
+	string NOME;
+	string endereco;
+	string Cep;
+	
+	void print(); // imprime na tela os dados de um cliente cadastrado
 };
 
 #endif
