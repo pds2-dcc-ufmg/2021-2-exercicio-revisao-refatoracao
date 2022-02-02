593b8925ac718587053895519f509251b0acba37
Lucas Duarte <50371525+duartegit@users.noreply.github.com>
2022-02-01 21:07:17 -0300

    Update Especialista.cpp

--- Especialista.cpp
+++ Especialista.cpp
@@ -3,3 +3,15 @@
 #include "Especialista.hpp"
 
 
+double Especialista::comissao(double valorVenda){
+        double c = valorVenda*perc;
+        return c;
+}
+
+string getEspecialidade(){
+  return this->especialidade;
+}
+void setEspecialidade(string especialidadeNovo){
+  this->especialidade = especialidadeNovo;
+}
+
