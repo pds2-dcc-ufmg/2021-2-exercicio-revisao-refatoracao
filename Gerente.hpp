b4fc6455837bb48c5bbb3815313870391bdaa8cd
Pedro-lucasf <98771738+Pedro-lucasf@users.noreply.github.com>
2022-02-01 21:32:28 -0300

    Update Gerente.hpp
    Coloquei as variaveis no private, e adicionei metodos set e get para cada variavel.

--- Gerente.hpp
+++ Gerente.hpp
@@ -9,16 +9,32 @@ const double Valor_Bonificacao = 15.0;
 
 class Gerente {
     public:
+        void print();
+
+        double calcula_BONIFICACAO_GERENTE(int Total_Vendas);
+        
+        void set_SalarioBase(double _SalarioBase);
+		double get_SalarioBase();
+		
+		void set_IDADE(std::string _IDADE);
+		std::string get_IDADE();
+		
+		void set_nome(std::string _nome);
+		std::string get_nome();
+		
+		void set_rgFunc(int _rgFunc);
+		int get_rgFunc();
+		
+		void set_bonificacao(double _bonificacao);
+		double get_bonificacao();
+		
+    private: 
         double SalarioBase;
         std::string IDADE;
         std::string nome;
         int rgFunc;
         double bonificacao;
 
-
-        void print();
-
-        double calcula_BONIFICACAO_GERENTE(int Total_Vendas);
 };
 
 #endif
