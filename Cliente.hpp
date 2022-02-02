f297357fb8d81b9c6537fa331f126b4c90b7e1c0
Pedro-lucasf <98771738+Pedro-lucasf@users.noreply.github.com>
2022-02-01 21:26:21 -0300

    Update Cliente.hpp
    Coloquei as variaveis no private, adicionei metodos set e get no public para cada variavel

--- Cliente.hpp
+++ Cliente.hpp
@@ -7,12 +7,21 @@
 class Cliente{
 
 	public:
-
+		void print();
+		
+		void set_NOME(std::string _nome);
+		std::string get_NOME();
+		
+		void set_Endereco(std::string _endereco);
+		std::string get_Endereco();
+		
+		void set_Cep(std::string _cep);
+		std::string get_Cep();
+		
+    private:
 		std::string NOME;
 		std::string endereco;
 		std::string Cep;
-
-		void print();
 
 };
 
