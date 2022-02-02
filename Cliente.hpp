bb3094a00efc26f7cc4c5617489965cecda57333
Kurotaki-G <81437746+Kurotaki-G@users.noreply.github.com>
2022-02-01 23:08:50 -0300

    Nomes: Underline e Low Case
    Nomes de variáveis padronizadas em minúsculo e separação em underline.

--- Cliente.hpp
+++ Cliente.hpp
@@ -6,9 +6,9 @@ using namespace std;
 class Cliente{
 
     public:
-	string NOME;
+	string nome;
 	string endereco;
-	string Cep;
+	string cep;
 	
 	void print(); // imprime na tela os dados de um cliente cadastrado
 };
