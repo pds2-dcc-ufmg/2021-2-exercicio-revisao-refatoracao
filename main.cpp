2c20d1a24206cd45cea1745b9e3a08abb290a805
brunapace <brunap_1901@hotmail.com>
2022-02-01 21:38:55 -0300

    Criando vetor de especialistas e renomeando g01 para gerente

--- main.cpp
+++ main.cpp
@@ -23,43 +23,43 @@ int main()
         it.print();
     }
 
-    Especialista e01  = Especialista(3000,"46","Peter Parker", 27061975, "Fotografia", 0, 0);
+    std::vector<Especialista> especialistas;
 
-    Especialista e02 = Especialista(1000,"56","Tony Stark", 4041965, "Consertos de equipamentos eletronicos", 0, 0);
+    especialistas.push_back(Especialista(3000,"46","Peter Parker", 27061975, "Fotografia", 0, 0));
+    especialistas.push_back(Especialista(1000,"56","Tony Stark", 4041965, "Consertos de equipamentos eletronicos", 0, 0));
+    especialistas.push_back(Especialista(5000,"32","Wanda Maximoff", 16021989, "Engenharia e Designeeeeer", 0, 0));
 
-    Especialista e03 = Especialista(5000,"32","Wanda Maximoff", 16021989, "Engenharia e Designeeeeer", 0, 0);
-
-    Gerente g01 (10000, "72", "Nick Fury", 21121948, 0);
-
-
-    Venda v01 (e01, 100, "Fotos do Homem Aranha", clientes[0]);
-    e01.efetuaVenda(v01._valor);
+    Gerente gerente (10000, "72", "Nick Fury", 21121948, 0);
 
 
-    Venda v02 (e02, 100, "Troca da tela do telefone", clientes[3]);
-    e02.efetuaVenda(v02._valor);
-
-    Venda v03(e01, 150, "Fotos do novo planador", clientes[1]);
-    e01.efetuaVenda(v03._valor);
+    Venda v01 (especialistas[0], 100, "Fotos do Homem Aranha", clientes[0]);
+    especialistas[0].efetuaVenda(v01._valor);
 
 
-    Venda v04(e02, 10, "Recarga de cartucho", clientes[0]);
-    e02.efetuaVenda(v04._valor);
+    Venda v02 (especialistas[1], 100, "Troca da tela do telefone", clientes[3]);
+    especialistas[1].efetuaVenda(v02._valor);
+
+    Venda v03(especialistas[0], 150, "Fotos do novo planador", clientes[1]);
+    especialistas[0].efetuaVenda(v03._valor);
 
 
-    Venda v05(e03, 10000, "Reconstrucao de Predio", clientes[3]);
-    e03.efetuaVenda(v05._valor);
+    Venda v04(especialistas[1], 10, "Recarga de cartucho", clientes[0]);
+    especialistas[1].efetuaVenda(v04._valor);
 
 
-    Venda v06(e03, 3000, "Decoracao de Apartamento no Brooklyn", clientes[4]);
-    e03.efetuaVenda(v06._valor);
-
-    Venda v07 (e03, 5000, "Reforma do Clarim Diario", clientes[0]);
-    e03.efetuaVenda(v07._valor);
+    Venda v05(especialistas[2], 10000, "Reconstrucao de Predio", clientes[3]);
+    especialistas[2].efetuaVenda(v05._valor);
 
 
-    Venda v08 (e02, 80, "Formatacao do PC", clientes[2]);
-    e02.efetuaVenda(v08._valor);
+    Venda v06(especialistas[2], 3000, "Decoracao de Apartamento no Brooklyn", clientes[4]);
+    especialistas[2].efetuaVenda(v06._valor);
+
+    Venda v07 (especialistas[2], 5000, "Reforma do Clarim Diario", clientes[0]);
+    especialistas[2].efetuaVenda(v07._valor);
+
+
+    Venda v08 (especialistas[1], 80, "Formatacao do PC", clientes[2]);
+    especialistas[1].efetuaVenda(v08._valor);
 
     cout <<" \n \n           Relatorio das Vendas \n" << endl;
 
@@ -83,14 +83,15 @@ int main()
 
     
     cout <<" \n \n           Relatorio dos Funcionarios \n" << endl;
-    e01.print();
-    e02.print();
-    e03.print();
 
     int numTotalServicos = 0;
-    numTotalServicos = e01._numAtendimentos + e02._numAtendimentos + e03._numAtendimentos;
-    g01._bonificacao = g01.calculaBonificacaoGerente(numTotalServicos);
-    g01.print();
+    for (auto it: especialistas){
+        it.print();
+        numTotalServicos+=it._numAtendimentos;
+    }
+
+    gerente._bonificacao = gerente.calculaBonificacaoGerente(numTotalServicos);
+    gerente.print();
 
     return 0;
 }
