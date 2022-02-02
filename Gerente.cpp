29428c1beffc5031c34610bbce55959d54eb7cb6
jao-07 <98784264+jao-07@users.noreply.github.com>
2022-02-01 23:23:57 -0300

    Update Gerente.cpp

--- Gerente.cpp
+++ Gerente.cpp
@@ -1,1 +1,20 @@
+#include "Gerente.hpp"
 
+Gerente::Gerente(double salarioBase, string idade, string nome, int RG, string bonificacao){
+  this->salarioBase = salarioBase;
+  this->idade = idade;
+  this->nome = nome;
+  this->RG = RG;
+  this->bonificacao = bonificacao;
+}
+
+void Gerente::print() const{
+  
+  cout << "[Funcionario]" << endl 
+   << "[Gerente]" << endl
+   print_dados();
+}
+
+double Gerente::calculoBonificacao(int numVendas){
+  return numVendas * BONIFICACAO;
+}
