b62ce25d9b7adf0a0c03291427585c5113138d6a
samuelrebula <84922393+samuelrebula@users.noreply.github.com>
2022-02-01 21:03:48 -0300

    Update Cliente.cpp

--- Cliente.cpp
+++ Cliente.cpp
@@ -1,13 +1,33 @@
 #include <iostream>
 #include <string>
+
 #include "Cliente.hpp"
 
-void Cliente::print(){
+Cliente::Cliente(){}
 
-	std::cout << "[Cliente]" << endl
-         << "  Nome: " << NOME << endl
-		 << "  Endereco: " << endereco << endl
-		 << "  CEP: " << Cep << endl;
+Cliente::Cliente(std::string nome, std::string endereco, std::string cep) {
+	this->_nome = nome;
+	this->_endereco = endereco;
+	this->_cep = cep;
+} 
 
+
+std::string Cliente::getNome(){
+	return this->_nome;
 }
 
+std::string Cliente::getCep(){
+	return this->_cep;
+}
+
+std::string Cliente::getEndereco(){
+	return this->_endereco;
+}
+
+
+void Cliente::print(){
+	std::cout << "[Cliente]" << std::endl
+		<< "  Nome: " << this->_nome << std::endl
+		<< "  Endereco: " << this->_endereco << std::endl
+		<< "  CEP: " << this->_cep << std::endl;
+}
