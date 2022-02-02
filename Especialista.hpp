bac901d6421d516880a4f7267f8877b105a89cec
Santanagc <98853626+Santanagc@users.noreply.github.com>
2022-02-01 22:25:32 -0300

    Update Especialista.hpp
    ----Alterações:----
    1.Remoção do "public" em desuso, desnecessário
    2.Identação melhor das linhas do código
    3.Remoção de "std" desnecessário
    4.Padronização(ou formatação) dos nomes das variáveis]
    5.Remoção do "#include"Cliente.hpp"", pois não está sendo usado, é desnecessário

--- Especialista.hpp
+++ Especialista.hpp
@@ -7,33 +7,32 @@
 using namespace std;
 
 double perc = 0.1;
-double percWanda = 0.1;
+double perc_wanda = 0.1;
 
-
-class Especialista : public Funcionario {
-
-    public:
-
+class Especialista : public Funcionario{
     public:
         string especialidade;
 
-    double comissao(double ValorVenda) {
-        double c = ValorVenda*perc;
-                  return c;
-    }
+        double comissao(double valor_venda){
+            double c = valor_venda*perc;
+            return c;
+        }
 
-
-    void print() {
-
-        std::cout << "[Especialista]" << endl;
-        Funcionario::print();
-
-
-
-        std::cout << "  Nome: " << nome << endl
-        << "  SalarioBase: R$ " << fixed << setprecision(2) << SalarioBase <<endl;
-
-    }
+        void print(){
+            cout << "[Especialista]" << endl;
+            Funcionario::print();
+            cout << "  Nome: " << nome << endl;
+            cout << "  SalarioBase: R$ " << fixed << setprecision(2) << salario_base <<endl;
+        }
 };
 
 #endif
+
+/**
+----Alterações:----
+1.Remoção do "public" em desuso, desnecessário
+2.Identação melhor das linhas do código
+3.Remoção de "std" desnecessário
+4.Padronização(ou formatação) dos nomes das variáveis]
+5.Remoção do "#include"Cliente.hpp"", pois não está sendo usado, é desnecessário
+**/
