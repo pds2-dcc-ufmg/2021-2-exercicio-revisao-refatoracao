8e363d21a75467ba742ebaf3a0c295e140b3c652
gccostaa <94900894+gccostaa@users.noreply.github.com>
2022-02-01 23:14:34 -0300

    Update Cliente.hpp

--- Cliente.hpp
+++ Cliente.hpp
@@ -10,12 +10,7 @@ class Cliente{
 		string Nome, Endereco, Cep, AlturaDosPais;
 		
 		// imprime na tela os dados de um cliente cadastrado
-		void print() {
-			std::cout << " Nome: " << Nome << std::endl;
-			std::cout << " Endereço: " << Endereco << std::endl;
-			std::cout << " CEP: " << Cep << std::endl;
-			std::cout << " Altura dos pais: " << AlturaDosPais << std::endl;
-		} 
+		void print(); 
 };
 
 #endif
