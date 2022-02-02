a21f922b4a038b6926c8ef89dfef90fcc5ec0464
Pedro-lucasf <98771738+Pedro-lucasf@users.noreply.github.com>
2022-02-01 21:31:21 -0300

    Update Funcionario.hpp
    Coloquei as variaveis no private, e adicionei metodos set e get para cada variavel.

--- Funcionario.hpp
+++ Funcionario.hpp
@@ -7,12 +7,25 @@
 
 class Funcionario {
     public:
+        void print();
+        
+        void set_SalarioBase(double _SalarioBase);
+		double get_SalarioBase();
+		
+		void set_IDADE(std::string _IDADE);
+		std::string get_IDADE();
+		
+		void set_nome(std::string _nome);
+		std::string get_nome();
+		
+		void set_rgFunc(int _rgFunc);
+		int get_rgFunc();
+		
+    private:
         double SalarioBase;
         std::string IDADE;
         std::string nome;
         int rgFunc;
-
-        void print();
 };
 
 #endif
