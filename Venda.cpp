8467557ff201b652063107813921a80eed139624
jao-07 <98784264+jao-07@users.noreply.github.com>
2022-02-01 23:57:11 -0300

    Update Venda.cpp

--- Venda.cpp
+++ Venda.cpp
@@ -1,1 +1,14 @@
+#include "Venda.hpp"
 
+Venda::Venda(double valor, string descricao, Especialista esp, Cliente cl){
+    this->valor = valor;
+    this->descricao = descricao;
+    this->esp = esp;
+    this->cl = cl;
+}
+
+void Venda::print() const{
+  cout << "Especialista: " << esp.nome << endl
+  << " Cliente: " << cl.nome << endl
+  << " Descricao: " << descricao << endl;
+}
