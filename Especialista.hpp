6cc5ac4c6f8228887e14a814df8b74c45449c20f
Haroldo Neto <60106977+Dodooneto@users.noreply.github.com>
2022-02-01 23:31:55 -0300

    Update Especialista.hpp
    implementei o setEspecialidade

--- Especialista.hpp
+++ Especialista.hpp
@@ -18,24 +18,32 @@ class Especialista : public Funcionario {
             << "  Idade: " << idade << endl
             << "  RGFunc: " << rgFunc << endl
             << "  SalarioBase: R$ " << fixed << setprecision(2) << salarioBase <<endl;
-        }
+        };
         /**
         *@brief Calcula o valor da renda extra a ser recebida
         *@param valorVenda Valor da Venda
         */
         double calculaExtra(double valorVenda) override {
             return valorVenda*percComissao;
-        } 
+        };
         /**
         *@brief Define o valor da renda extra a ser recebida
         *@param valorVenda Valor da Venda
         */   
         void setRendaExtra(double valorVenda) {
             rendaExtra =  calculaExtra(valorVenda);   
-        }
+        };
+        /**
+        *@brief Define a especialidade do funcionário
+        *@param especialidadeFuncionario Especialidade do funcionário
+        */   
+        void setEspecialidade(string especialidadeFuncionario){
+            especialidade = especialidadeFuncionario;
+        };
 
     private:
         percComissao = 0.1;
+        string especialidade;
 };
 
 #endif
