93415ec58dd3990a0e644c46bf07eac303de2d12
Lucas Duarte <50371525+duartegit@users.noreply.github.com>
2022-02-01 22:53:34 -0300

    Update Especialista.hpp

--- Especialista.hpp
+++ Especialista.hpp
@@ -7,15 +7,16 @@ using namespace std;
 
 class Especialista : public Funcionario {
     private:
+            double valorComissao;
             double perc;
             string especialidade;
     
     public:
             double comissao(double ValorVenda);
-
+            void setComissao(double valorComissaoNovo);
             string getEspecialidade();
             void setEspecialidade(string especialidadeNovo);
-    
+            
             void print();
 };
 
