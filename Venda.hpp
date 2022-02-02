6db1cbbb15882069e1e6f654b80b498af88638c0
Haroldo Neto <60106977+Dodooneto@users.noreply.github.com>
2022-02-01 23:22:28 -0300

    Update Venda.hpp
    Adicionei comentários e novos modulos

--- Venda.hpp
+++ Venda.hpp
@@ -11,32 +11,52 @@ using namespace std;
 class Venda{
 
     public:
-    double VALOR;
-    string descricao;
-    Especialista esp;
-    string cliente;
+        /**
+        *@brief Imprime as informações fundamentais da venda (funcionario, cliente e descricao)
+        */   
+        void print();
+         /**
+        *@brief Define as variaveis da venda
+        *@param valor Valor da venda
+        *@param descricao Descricao da venda
+        *@param especialista Especialista responsável pela venda
+        *@param cliente Cliente da venda
+        */      
+        void setDados(double valor, string descricao, Especialista especialista, Cliente cliente);
+        /**
+        *@brief Retorna o valor da venda
+        */
+        double getValor();
+        /**
+        *@brief Retorna a descrição da venda
+        */
+        string getDescricao();
+        /**
+        *@brief Retorna o especialista responsável pela venda
+        */
+        Especialista getEspecialista();
+        /**
+        *@brief Retorna o cliente da venda
+        */
+        Cliente getCliente();
 
-
-    void print() {
-
-        std::cout << "Especialista: ";
-        cout << esp.nome;
-
-        std::cout << " Cliente: ";
-        cout << cliente;
-
-        //std::cout << " : ";
-        //cout << cliente;
-    }
-
-    /*void print() {
-
-        std::cout << "Especialista: ";
-        cout << esp.nome;
-
-        std::cout << " Cliente: ";
-        cout << cliente;
-    }*/
+    private:
+        /**
+        *@brief Valor da venda
+        */
+        double valor;
+        /**
+        *@brief Descrição da venda
+        */
+        string descricao;
+        /**
+        *@brief Especialista responsável pela venda
+        */
+        Especialista especialista;
+        /**
+        *@brief Cliente da venda
+        */
+        Cliente cliente;
 };
 
 #endif
