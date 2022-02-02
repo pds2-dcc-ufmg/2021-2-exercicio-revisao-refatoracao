c9bd406b4b1c9ba386f761278e3d6b41e05cbd8b
Pedro-lucasf <98771738+Pedro-lucasf@users.noreply.github.com>
2022-02-01 21:39:43 -0300

    Update Funcionario.cpp
    Adicionei a implementação dos metodos get e set de cada variavel do hpp, e substitui as variaveis no print pelos metodos get

--- Funcionario.cpp
+++ Funcionario.cpp
@@ -2,6 +2,39 @@
 
 void Funcionario::print() {
             std::cout << "[Funcionario]" << std::endl
-            << "  Idade: " << IDADE << std::endl
-            << "  RGFunc: " << rgFunc << std::endl;
+            << "  Idade: " << get_IDADE() << std::endl
+            << "  RGFunc: " << get_rgFunc() << std::endl;
         }
+
+        
+void Funcionario::set_SalarioBase(double _SalarioBase){
+    SalarioBase = _SalarioBase;
+}
+
+double Funcionario::get_SalarioBase(){
+    return SalarioBase;
+}
+		
+void Funcionario::set_IDADE(std::string _IDADE){
+    IDADE = _IDADE;
+}
+
+std::string Funcionario::get_IDADE(){
+    return IDADE;
+}
+		
+void Funcionario::set_nome(std::string _nome){
+    nome = _nome;
+}
+
+std::string Funcionario::get_nome(){
+    return nome;
+}
+		
+void Funcionario::set_rgFunc(int _rgFunc){
+    rgFunc = _rgFunc;
+}
+
+int Funcionario::get_rgFunc(){
+    return rgFunc;
+}
