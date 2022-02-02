0fabc67c61a947df0be99104189a189a9e7342e6
Pedro-lucasf <98771738+Pedro-lucasf@users.noreply.github.com>
2022-02-01 21:37:29 -0300

    Update Especialista.cpp
    Adicionei a implementação dos metodos get e set de cada variavel do hpp, e substitui as variaveis no print pelos metodos get

--- Especialista.cpp
+++ Especialista.cpp
@@ -5,8 +5,8 @@ void Especialista::print(){
         std::cout << "[Especialista]" << std::endl;
         Funcionario::print();
 
-        std::cout << "  Nome: " << nome << std::endl
-        << "  SalarioBase: R$ " << std::fixed << std::setprecision(2) << SalarioBase <<std::endl;
+        std::cout << "  Nome: " << get_nome() << std::endl
+        << "  SalarioBase: R$ " << std::fixed << std::setprecision(2) << get_SalarioBase() <<std::endl;
 
     }
     
@@ -14,3 +14,11 @@ double Especialista::comissao(double ValorVenda){
         double _Comissao = ValorVenda*perc;
                   return _Comissao;
     }
+
+void Especialista::set_especialidade(std::string _especialidade){
+    especialidade = _especialidade;
+}
+
+std::string Especialista::get_especialidade(){
+    return especialidade;
+}
