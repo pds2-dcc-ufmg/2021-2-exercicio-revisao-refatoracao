7ff4adcea582fcb4ff7cad271f075194873c755b
Kurotaki-G <81437746+Kurotaki-G@users.noreply.github.com>
2022-02-01 22:02:01 -0300

    Update Gerente - herança no método print()
    Método print() da superclasse possui elementos em comum.
    Toda a repetição foi substituído por  Funcionario::print(); para aproveitar o máximo o reúso

--- Gerente.hpp
+++ Gerente.hpp
@@ -16,12 +16,8 @@ class Gerente : public Funcionario{
     public:
     
         void print() {
-            cout << "[Funcionario]" << endl
-            << "[Gerente]" << endl
-            << "  Nome: " << nome << endl
-            << "  Idade: " << IDADE << endl
-            << "  RGFunc: " << rgFunc << endl
-            << "  SalarioBase: R$ " << fixed << setprecision(2) << SalarioBase <<endl;
+            cout << "[Gerente]" << endl;
+           Funcionario::print();
         }
 
         double calcula_BONIFICACAO_GERENTE(int numTOTALVendas){
