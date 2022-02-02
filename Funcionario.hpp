df2f33f4b9601bc09610c03a8f2be3810e433c67
Emily Oliveira dos Santos <98438743+EmilyOliSan@users.noreply.github.com>
2022-02-01 23:50:48 -0300

    Create Funcionario.hpp

---
+++ Funcionario.hpp
@@ -0,0 +1,23 @@
+#ifndef FUNCIONARIO_HPP
+#define FUNCIONARIO_HPP
+
+#include <iostream>
+#include <iomanip>
+
+class Resgistro {
+    
+    public:
+        std::string idade;
+        int rgFuncionario;        
+        double salarioBase;
+        std::string nome;
+};
+
+class Funcionario : public Resgistro {
+
+    public:
+
+        void print();
+        void print_oi();
+};
+#endif
