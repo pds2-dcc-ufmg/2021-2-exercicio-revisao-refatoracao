e511afe53a2455f5fcb53cfea2ed360c811e3cce
Emily Oliveira dos Santos <98438743+EmilyOliSan@users.noreply.github.com>
2022-02-01 23:51:47 -0300

    Create Cliente.hpp

---
+++ Cliente.hpp
@@ -0,0 +1,25 @@
+#ifndef CLIENTE_H
+#define CLIENTE_H
+
+#include <string>
+class Cliente{
+	
+	public:
+
+		void print_cliente(); // imprime na tela os dados de um cliente cadastrado
+		std::string nome;
+        std::string endereco;
+		std::string cep;
+		void setNome(std::string);
+        std::string getNome();
+        void setEndereco(std::string);
+        std::string getEndereco();
+		void setCep(std::string);
+        std::string getCep();
+		
+	private:
+	
+		std::string AlturaDosPais;
+};
+
+#endif
