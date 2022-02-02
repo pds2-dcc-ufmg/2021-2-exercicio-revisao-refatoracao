ab44a160b39dab0db2a66ddf51d1646b500672e4
Pedro-lucasf <98771738+Pedro-lucasf@users.noreply.github.com>
2022-02-01 21:35:54 -0300

    Update Cliente.cpp
    Adicionei a implementação dos metodos get e set de cada variavel do hpp

--- Cliente.cpp
+++ Cliente.cpp
@@ -3,8 +3,32 @@
 void Cliente::print(){
 
 	std::cout << "[Cliente]" << std::endl
-         << "  Nome: " << NOME << std::endl
-		 << "  Endereco: " << endereco << std::endl
-		 << "  CEP: " << Cep << std::endl;
+         << "  Nome: " << get_NOME() << std::endl
+		 << "  Endereco: " << get_Endereco() << std::endl
+		 << "  CEP: " << get_Cep() << std::endl;
 
+}
+
+void Cliente::set_NOME(std::string _nome){
+    NOME = _nome;
+}
+
+std::string Cliente::get_NOME(){
+    return NOME;
+}
+
+void Cliente::set_Endereco(std::string _endereco){
+    endereco = _endereco;
+}
+
+std::string Cliente::get_Endereco(){
+    return endereco;
+}
+		
+void Cliente::set_Cep(std::string _cep){
+    Cep = _cep;
+}
+
+std::string Cliente::get_Cep(){
+    return Cep;
 }
