ac1dabfeedb9e3dad48b863182d34399a543f029
Pedro-lucasf <98771738+Pedro-lucasf@users.noreply.github.com>
2022-02-01 21:41:18 -0300

    Update Venda.cpp
    Adicionei a implementação dos metodos get e set de cada variavel do hpp, e substitui as variaveis no print pelos metodos get

--- Venda.cpp
+++ Venda.cpp
@@ -3,8 +3,39 @@
 void Venda::print() {
 
         std::cout << "Especialista: ";
-        std::cout << esp.nome;
+        std::cout << esp.get_nome();
 
         std::cout << " Cliente: ";
-        std::cout << cliente;
+        std::cout << get_cliente();
     }
+void Venda::set_VALOR(double _VALOR){
+    VALOR = _VALOR;
+}
+
+double Venda::get_VALOR(){
+    return VALOR;
+}
+		
+void Venda::set_descricao(std::string _descricao){
+    descricao = _descricao;
+}
+
+std::string Venda::get_descricao(){
+    return descricao;
+}
+	
+void Venda::set_esp(Especialista _esp){
+    esp = _esp;
+}
+
+Especialista Venda::get_esp(){
+   return esp; 
+}
+		
+void Venda::set_cliente(std::string _cliente){
+    cliente = _cliente;
+}
+
+std::string Venda::get_cliente(){
+    return cliente;
+}
