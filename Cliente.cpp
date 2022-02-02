2afb81fa3022e51b85498afc22af76655f17b901
Santanagc <98853626+Santanagc@users.noreply.github.com>
2022-02-01 22:24:49 -0300

    Update Cliente.cpp
    ----Alterações:----
    1.Identação melhor das linhas do código
    2.Uso do "using namespace std" para facilitação no uso da linguagem
    3.Padronização(ou formatação) dos nomes das variáveis
    4.Inserção do "this" para especificar qual o atributo

--- Cliente.cpp
+++ Cliente.cpp
@@ -1,13 +1,20 @@
 #include <iostream>
 #include <string>
-#include "Cliente.hpp"
+#include "Cliente.hpp
+
+using namespace std;
 
 void Cliente::print(){
-
-	std::cout << "[Cliente]" << endl
-         << "  Nome: " << NOME << endl
-		 << "  Endereco: " << endereco << endl
-		 << "  CEP: " << Cep << endl;
-
+	cout << "[Cliente]" << endl;
+	cout << "  Nome: " << this->nome << endl;
+	cout << "  Endereco: " << this->endereco << endl;
+	cout << "  CEP: " << this->cep << endl;
 }
 
+/**
+----Alterações:----
+1.Identação melhor das linhas do código
+2.Uso do "using namespace std" para facilitação no uso da linguagem
+3.Padronização(ou formatação) dos nomes das variáveis
+4.Inserção do "this" para especificar qual o atributo
+**/
