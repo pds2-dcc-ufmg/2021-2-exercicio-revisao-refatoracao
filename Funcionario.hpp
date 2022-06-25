71b359c58b4f8a4b749aa7d02fac617c7088abc1
becaroline <88947044+becaroline@users.noreply.github.com>
2022-06-25 18:44:07 -0300

    Update Funcionario.hpp

--- Funcionario.hpp
+++ Funcionario.hpp
@@ -2,17 +2,28 @@
 #define FUNCIONARIO_HPP
 
 #include <iostream>
-#include <string>
+#include <iomanip>
 
 using namespace std;
 
-class Funcionario {
-  
-    public:
-     int rg;
-     int idade;
-     string nome;
-     double salario_base; // valor minimo recebido pelo funcionario
+class Funcionario
+{
+public:
+    void setSalarioBase(double);
+    double getSalarioBase();
+    void setIdade(int);
+    int getIdade();
+    void setNome(string);
+    string getNome();
+    void setRgFunc(int);
+    int getRgFunc();
+    void print();
+
+private:
+    double _salario_base;
+    int _idade;
+    string _nome;
+    int _rg_func;
 };
 
 #endif
