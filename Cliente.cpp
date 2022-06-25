4452a5a2cdd62bb1b598a2bdf561052f62eb3b74
becaroline <88947044+becaroline@users.noreply.github.com>
2022-06-25 18:41:12 -0300

    Update Cliente.cpp

--- Cliente.cpp
+++ Cliente.cpp
@@ -1,18 +1,43 @@
-#include "Cliente.hpp"
-
 #include <iostream>
 #include <string>
+#include "Cliente.hpp"
 
-Cliente::Cliente(string nome, string endereco, string cep){
-  this->endereco = endereco;
-  this->nome = nome;
-  this->cep = cep;
+using namespace std;
+
+void Cliente::setNome(string nome)
+{
+	this->_nome = nome;
 }
 
+string Cliente::getNome()
+{
+	return _nome;
+}
 
-void Cliente::print(){
-    cout << "[Cliente]" << endl
-         << "  Nome: " << nome << endl
-		     << "  Endereco: " << endereco << endl
-		     << "  CEP: " << cep << endl;
-    }
+void Cliente::setEndereco(string endereco)
+{
+	this->_endereco = endereco;
+}
+
+string Cliente::getEndereco()
+{
+	return _endereco;
+}
+
+void Cliente::setCep(string cep)
+{
+	this->_cep = cep;
+}
+
+string Cliente::getCep()
+{
+	return _cep;
+}
+
+void Cliente::print()
+{
+	cout << "[Cliente]" << endl
+			  << "  Nome: " << _nome << endl
+			  << "  Endereco: " << _endereco << endl
+			  << "  CEP: " << _cep << endl;
+}
