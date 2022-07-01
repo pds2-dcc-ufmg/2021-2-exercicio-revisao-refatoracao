f45ae8ea203b888c0326a8ca653b5a84c11c1dec
isadorahdc <104742053+isadorahdc@users.noreply.github.com>
2022-06-30 21:18:04 -0300

    Create Funcionario.cpp

---
+++ Funcionario.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <iomanip>
+#include "Funcionario.hpp"
+
+void print() {
+            cout << "[Funcionario]" << endl
+            << "  Idade: " << IDADE << endl
+            << "  RGFunc: " << rgFunc << endl;
+}
+
+void print_oi(){
+            cout << "Tchau" << endl;
+}
+
