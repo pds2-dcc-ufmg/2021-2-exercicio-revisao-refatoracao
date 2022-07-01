62c26909006ec799c876d937eb97168b27411773
isadorahdc <104742053+isadorahdc@users.noreply.github.com>
2022-06-30 21:29:55 -0300

    Create Venda.cpp

---
+++ Venda.cpp
@@ -0,0 +1,15 @@
+#include "Funcionario.hpp"
+#include "Cliente.hpp"
+#include "Especialista.hpp"
+#include "Gerente.hpp"
+
+
+void print() {
+
+        std::cout << "Especialista: ";
+        std::cout << esp.nome;
+
+        std::cout << " Cliente: ";
+        std::cout << cliente;
+
+    }
