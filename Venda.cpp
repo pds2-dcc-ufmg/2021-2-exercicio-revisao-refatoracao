6109736479134e8b531a6fe1f921043c5d41d509
nicectp <108739137+nicectp@users.noreply.github.com>
2022-07-06 15:26:44 -0300

    Delete Venda.cpp

--- Venda.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-#include <string>
-#include "Venda.hpp"
-
-void Venda::print() {
-    std::cout << "Especialista: " << especialista.nome;
-    std::cout << " Cliente: " << cliente;
-}
