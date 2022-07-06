ced8e21b78dbea6767d1d5b99ede722c24436a3e
nicectp <108739137+nicectp@users.noreply.github.com>
2022-07-06 13:43:12 -0300

    Update Gerente.hpp

--- Gerente.hpp
+++ Gerente.hpp
@@ -4,34 +4,22 @@
 #include <iostream>
 #include <iomanip>
 
+#include "Funcionario.hpp"
+
 using namespace std;
 
-double ValorBONIFICACAO = 15.0;
+double valorBonus = 15.0;
 
-class Gerente {
+class Gerente: public Funcionario {
+    
     public:
-        double SalarioBase; // valor mínimo recebido pelo funcionário
-        string IDADE;
-        string nome;
-        int rgFunc;
-        double bonificacao;
-
-
-        void print() {
-            cout << "[Funcionario]" << endl
-            << "[Gerente]" << endl
-            << "  Nome: " << nome << endl
-            << "  Idade: " << IDADE << endl
-            << "  RGFunc: " << rgFunc << endl
-            << "  SalarioBase: R$ " << fixed << setprecision(2) << SalarioBase <<endl;
-        }
-
-        double calcula_BONIFICACAO_GERENTE(int numTOTALVendas){
-            double x;
-            return numTOTALVendas*ValorBONIFICACAO;
-        }
+    
+    double bonificacao;
+    
+    double calcula_bonus(int totalVendas);
+    
+    void print();
 
 };
 
 #endif
-
