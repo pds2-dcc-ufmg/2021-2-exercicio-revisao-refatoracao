6439a3ab249ffb3b5f09bb16ddbc070d9cb2f999
Hamilton1984 <152026053+Hamilton1984@users.noreply.github.com>
2023-11-25 15:30:27 -0300

    Update Venda.hpp

--- Venda.hpp
+++ Venda.hpp
@@ -1,42 +1,17 @@
-
 #ifndef VENDA_HPP
 #define VENDA_HPP
 
-#include "Funcionario.hpp"
-#include "Cliente.hpp"
 #include "Especialista.hpp"
+#include <iostream>
 
-using namespace std;
+class Venda {
+public:
+    double valor;
+    std::string descricao;
+    Especialista especialista;
+    std::string cliente;
 
-class Venda{
-
-    public:
-    double VALOR;
-    string descricao;
-    Especialista esp;
-    string cliente;
-
-
-    void print() {
-
-        std::cout << "Especialista: ";
-        cout << esp.nome;
-
-        std::cout << " Cliente: ";
-        cout << cliente;
-
-        //std::cout << " : ";
-        //cout << cliente;
-    }
-
-    /*void print() {
-
-        std::cout << "Especialista: ";
-        cout << esp.nome;
-
-        std::cout << " Cliente: ";
-        cout << cliente;
-    }*/
+    void print() const;
 };
 
 #endif
