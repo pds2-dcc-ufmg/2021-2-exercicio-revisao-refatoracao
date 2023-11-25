67319e70510de24c9ebe510348a05fef1cdff21c
Hamilton1984 <152026053+Hamilton1984@users.noreply.github.com>
2023-11-25 15:29:57 -0300

    Update Cliente.cpp

--- Cliente.cpp
+++ Cliente.cpp
@@ -1,9 +1,18 @@
 #include "Cliente.hpp"
 #include <iostream>
 
+void Cliente::setNome(const std::string& nome) {
+    this->nome = nome;
+}
+
+void Cliente::setEndereco(const std::string& endereco) {
+    this->endereco = endereco;
+}
+
+void Cliente::setCep(const std::string& cep) {
+    this->cep = cep;
+}
+
 void Cliente::print() const {
-    std::cout << "[Cliente]" << std::endl
-              << "  Nome: " << nome << std::endl
-              << "  Endereco: " << endereco << std::endl
-              << "  CEP: " << cep << std::endl;
+    std::cout << "Nome: " << nome << ", Endereco: " << endereco << ", CEP: " << cep << std::endl;
 }
