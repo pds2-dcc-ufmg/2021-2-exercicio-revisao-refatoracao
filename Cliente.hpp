c7eccfc5e8819a177d555318c0874954833c694d
Hamilton1984 <152026053+Hamilton1984@users.noreply.github.com>
2023-11-25 15:29:47 -0300

    Update Cliente.hpp

--- Cliente.hpp
+++ Cliente.hpp
@@ -2,13 +2,18 @@
 #define CLIENTE_HPP
 
 #include <string>
+#include <iostream>
 
 class Cliente {
-public:
+private:
     std::string nome;
     std::string endereco;
     std::string cep;
 
+public:
+    void setNome(const std::string& nome);
+    void setEndereco(const std::string& endereco);
+    void setCep(const std::string& cep);
     void print() const;
 };
 
