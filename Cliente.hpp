55ceffdbf4cb2f301e1f595f4c85d58a9029e54c
Pietro Ferreira Fernandes <146132485+pietroffernandes@users.noreply.github.com>
2023-11-26 15:36:53 -0300

    Update Cliente.hpp

--- Cliente.hpp
+++ Cliente.hpp
@@ -2,19 +2,26 @@
 #define CLIENTE_H
 
 #include <string>
-using namespace std;
 
-class Cliente{
-
+class Cliente {
 	public:
+		Cliente(std::string NOME, std::string endereco, std::string Cep): _NOME(NOME), _endereco(endereco), _Cep(Cep) {}
+	
+	        std::string getNome() const{
+	            return _NOME;
+	        }
+	        std::string getEndereco() const{
+	            return _endereco;
+	        }
+	        std::string getCep() const{
+	            return _Cep;
+	        }
+		void print() const; // imprime na tela os dados de um cliente cadastrado
 
-		string NOME;
-		string endereco;
-		string Cep;
-		string AlturaDosPais;
-
-		void print(); // imprime na tela os dados de um cliente cadastrado
-
+	private:
+	        std::string _NOME;
+		std::string _endereco;
+		std::string _Cep;
 };
 
 #endif
