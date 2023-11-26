5472be9e6e2eef4afed71a41f5566da1ceae8f09
Pietro Ferreira Fernandes <146132485+pietroffernandes@users.noreply.github.com>
2023-11-26 15:42:36 -0300

    Update Funcionario.hpp

--- Funcionario.hpp
+++ Funcionario.hpp
@@ -4,24 +4,35 @@
 #include <iostream>
 #include <iomanip>
 
-using namespace std;
-
 class Funcionario {
     public:
-        double SalarioBase; // valor mínimo recebido pelo funcionário
-        string IDADE;
-        string nome;
-        int rgFunc;
+        Funcionario(double SalarioBase, std::string IDADE, std::string nome, int rgFunc):
+                    _SalarioBase(SalarioBase), _IDADE(IDADE), _nome(nome), _rgFunc(rgFunc) {}
 
-        void print() {
-            cout << "[Funcionario]" << endl
-            << "  Idade: " << IDADE << endl
-            << "  RGFunc: " << rgFunc << endl;
+        double getSalarioBase() const{
+            return _SalarioBase;
+        }
+        std::string getIdade() const{
+            return _IDADE;
+        }
+        std::string getNome() const{
+            return _nome;
+        }
+        int getRG() const{
+            return _rgFunc;
         }
 
-        void print_oi(){
-            cout << "Tchau" << endl;
+        void print() const {
+            std::cout << "[Funcionario]" << std::endl
+            << "  Idade: " << this->getIdade() << std::endl
+            << "  RGFunc: " << this->getRG() << std::endl;
         }
+
+    private:
+        double _SalarioBase; // valor minimo recebido pelo funcionario
+        std::string _IDADE;
+        std::string _nome;
+        int _rgFunc;
 };
 
 #endif
