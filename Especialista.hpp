788e73b8c44886afe9ce6ed633804fb91977e7b7
gcc0sta <151790667+gcc0sta@users.noreply.github.com>
2023-11-27 20:03:09 -0300

    Update Especialista.hpp

--- Especialista.hpp
+++ Especialista.hpp
@@ -8,7 +8,7 @@ double percentual = 0.1;
 class Especialista : public Funcionario {
 private:
     std::string _especialidade;
-
+    double salario;
 public:
     Especialista(std::string& nome, int idade, int rgFuncionario, double salarioBase, std::string especialidade);
     double calcularComissao(double valorVenda);
