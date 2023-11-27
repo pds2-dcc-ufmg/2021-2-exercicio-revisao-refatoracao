c26faf258363625d6c971702722abd7151318980
gcc0sta <151790667+gcc0sta@users.noreply.github.com>
2023-11-27 19:28:15 -0300

    Update Funcionario.cpp

--- Funcionario.cpp
+++ Funcionario.cpp
@@ -3,10 +3,10 @@
 Funcionario::Funcionario (std::string& nome, int idade, int rgFuncionario, double salarioBase) :
 _nome(nome), _idade(idade), _rgFuncionario(rgFuncionario), _salarioBase(salarioBase) {}
 
-void Funcionario::imprimeDadosFuncionario() {
+virtual void Funcionario::imprimeDadosFuncionario() {
     std::cout << "[Funcionário]" << std::endl;
-	  std::cout << "Nome: " << _nome << std::endl;
-	  std::cout << "Idade: " << _idade << std::endl;
+    std::cout << "Nome: " << _nome << std::endl;
+    std::cout << "Idade: " << _idade << std::endl;
     std::cout << "RG: " << _rgFuncionario << std::endl;
     std::cout << "Salário: " << _salarioBase << std::endl;
 }
