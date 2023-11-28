d9af95359aa5617e5c61ff592444a9a03638c180
Atgot <152310870+Atgot@users.noreply.github.com>
2023-11-28 15:19:42 -0300

    Update Gerente.hpp
    Altered bonificacao implementation

--- Gerente.hpp
+++ Gerente.hpp
@@ -6,14 +6,13 @@
 
 using namespace std;
 
-double ValorBONIFICACAO = 15.0;
-
 class Gerente: public Funcionario {
     private:
         double _bonificacao;
+        double ValorBONIFICACAO = 15.0;
     public:
-        Gerente(string nome, string IDADE, int rgFunc, double SalarioBase)
-        Funcionario(nome, IDADE, rgFunc, SalarioBase) {}
+        Gerente(string nome, string IDADE, int rgFunc, double SalarioBase):
+        Funcionario(nome, IDADE, rgFunc, SalarioBase), _bonificacao=0 {}
         void print() {
             cout << "[Funcionario]" << endl
             << "[Gerente]" << endl
@@ -24,8 +23,8 @@ class Gerente: public Funcionario {
         }
 
         double calcula_BONIFICACAO_GERENTE(int numTOTALVendas){
-            double x;
-            return numTOTALVendas*ValorBONIFICACAO;
+            double _bonificacao=numTOTALVendas*ValorBONIFICACAO;
+            return _bonificacao;
         }
 
 };
