ffc3ff1e8b6cd63cf71300ed3621e3d50dcdab1d
Chr1s-J <103655149+Chr1s-J@users.noreply.github.com>
2023-11-28 10:52:25 -0300

    Update Cliente.cpp
    1) Deletado /* #include <string> */ desnecessário, pois está incluso no arquivo Cliente.hpp.
    
    2) Remoção de /* using namespace std; */ e atribuição dos devidos inclusões do namespace /* std:: */ .
    
    3) Alteração de espaçamento e justificação do código.

--- Cliente.cpp
+++ Cliente.cpp
@@ -1,13 +1,12 @@
 #include <iostream>
-#include <string>
+
 #include "Cliente.hpp"
 
 void Cliente::print(){
 
-	std::cout << "[Cliente]" << endl
-         << "  Nome: " << NOME << endl
-		 << "  Endereco: " << endereco << endl
-		 << "  CEP: " << Cep << endl;
+	std::cout << "[Cliente]" << std::endl
+        << "  Nome: " << NOME << std::endl
+	<< "  Endereco: " << endereco << std::endl
+	<< "  CEP: " << Cep << std::endl;
 
 }
-
