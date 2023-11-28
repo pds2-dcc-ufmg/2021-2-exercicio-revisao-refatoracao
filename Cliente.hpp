bc1fdfe3b152fd332ee323b07278ec0b4d0076ea
Atgot <152310870+Atgot@users.noreply.github.com>
2023-11-28 14:09:59 -0300

    Update Cliente.hpp
    Added Constructot

--- Cliente.hpp
+++ Cliente.hpp
@@ -5,14 +5,14 @@
 using namespace std;
 
 class Cliente{
-
+        private:
+                string _NOME;
+	        string _endereco;
+	        string _Cep;
+	        string _AlturaDosPais;
 	public:
-
-		string NOME;
-		string endereco;
-		string Cep;
-		string AlturaDosPais;
-
+		Handler(string Nome, string endereco, string Cep, string AlturaDosPais): 
+                _NOME(NOME), _endereco(endereco), _Cep(Cep), _AlturaDosPais(AlturaDosPais) {}
 		void print(); // imprime na tela os dados de um cliente cadastrado
 
 };
