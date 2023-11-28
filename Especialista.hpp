716e918720e6247947bf4b10756f2952461c75cb
Victor Thome <victor-thome@hotmail.com>
2023-11-28 19:05:13 -0300

    commit

--- Especialista.hpp
+++ Especialista.hpp
@@ -2,37 +2,22 @@
 #define Especialista_HPP
 
 #include "Funcionario.hpp"
-#include "Cliente.hpp"
 
 using namespace std;
 
-double perc = 0.1;
-double percWanda = 0.1;
-
-
 class Especialista : public Funcionario {
-
-    public:
-
-    public:
-        string especialidade;
-
+public:
+    string especialidade;
+    double porcentagem;
     double comissao(double ValorVenda) {
-        double c = ValorVenda*perc;
-                  return c;
+        double c = ValorVenda*porcentagem;
+        return c;
     }
-
-
     void print() {
-
-        std::cout << "[Especialista]" << endl;
+        cout << "[Especialista]" << endl;
         Funcionario::print();
-
-
-
-        std::cout << "  Nome: " << nome << endl
+        cout << "  Nome: " << nome << endl
         << "  SalarioBase: R$ " << fixed << setprecision(2) << SalarioBase <<endl;
-
     }
 };
 
