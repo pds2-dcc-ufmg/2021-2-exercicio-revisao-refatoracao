a5cb98838cdf95a0a17915ab2145770c9467b7e9
Atgot <152310870+Atgot@users.noreply.github.com>
2023-11-28 16:33:18 -0300

    Update Especialista.hpp
    Debugging

--- Especialista.hpp
+++ Especialista.hpp
@@ -13,7 +13,7 @@ class Especialista : public Funcionario {
         double _comissao;
     public:
         Especialista(string nome, string IDADE, int rgFunc, double SalarioBase, string especialidade):
-        Funcionario(nome, IDADE, rgFunc, SalarioBase), _especialidade(especialidade), _numAtendimentos=0, _comissao=0 
+        Funcionario(nome, IDADE, rgFunc, SalarioBase), _especialidade(especialidade), _numAtendimentos(0), _comissao(0) 
         {}
 
     void comissao(double ValorVenda) {
@@ -25,17 +25,40 @@ class Especialista : public Funcionario {
         return _numAtendimentos;
     }
     void print_nome() {
-        cout << nome << endl;
+        cout << _nome << endl;
     }
 
     void print() {
         cout << "[Especialista]" << endl;
         Funcionario::print();
-        cout << "  Nome: " << nome << endl
-        << "  SalarioBase: R$ " << fixed << setprecision(2) << SalarioBase << endl
+        cout << "  Nome: " << _nome << endl
+        << "  SalarioBase: R$ " << fixed << setprecision(2) << _SalarioBase << endl
         << "Num Atendimentos: " << _numAtendimentos << endl
         << "Salario Total: " << _SalarioBase+_comissao << endl;
     }
 };
 
+class Gerente: public Funcionario {
+    private:
+        double _bonificacao;
+        double ValorBONIFICACAO = 15.0;
+    public:
+        Gerente(string nome, string IDADE, int rgFunc, double SalarioBase):
+        Funcionario(nome, IDADE, rgFunc, SalarioBase), _bonificacao(0) {}
+        void print() {
+            cout << "[Funcionario]" << endl
+            << "[Gerente]" << endl
+            << "  Nome: " << _nome << endl
+            << "  Idade: " << _IDADE << endl
+            << "  RGFunc: " << _rgFunc << endl
+            << "  SalarioBase: R$ " << fixed << setprecision(2) << _SalarioBase << endl
+            << "Salario Total: " << _SalarioBase + _bonificacao << endl;
+        }
+
+        double calcula_BONIFICACAO_GERENTE(int numTOTALVendas){
+            double _bonificacao=numTOTALVendas*ValorBONIFICACAO;
+            return _bonificacao;
+        }
+
+};
 #endif
