000d8d68fb2602ac10735a48efab46cd8c38000c
Atgot <152310870+Atgot@users.noreply.github.com>
2023-11-28 16:35:24 -0300

    Update Cliente.hpp
    Debugging

--- Cliente.hpp
+++ Cliente.hpp
@@ -6,15 +6,14 @@ using namespace std;
 
 class Cliente{
         private:
-                string _NOME;
+            string _NOME;
 	        string _endereco;
 	        string _Cep;
 	        string _AlturaDosPais;
 	public:
-		Cliente(string Nome, string endereco, string Cep, string AlturaDosPais): 
-                _NOME(NOME), _endereco(endereco), _Cep(Cep), _AlturaDosPais(AlturaDosPais) {}
-		void print(); // imprime na tela os dados de um cliente cadastrado
-
+		Cliente(string NOME, string endereco, string Cep): 
+                _NOME(NOME), _endereco(endereco), _Cep(Cep) {}
+		void print(){}; // imprime na tela os dados de um cliente cadastrado
 };
 
 #endif
