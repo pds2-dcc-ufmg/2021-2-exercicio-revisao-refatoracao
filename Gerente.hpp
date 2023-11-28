1af78abae2c537cba256b09203f6a97dac68670f
gcc0sta <151790667+gcc0sta@users.noreply.github.com>
2023-11-28 07:53:35 -0300

    Update Gerente.hpp

--- Gerente.hpp
+++ Gerente.hpp
@@ -1,36 +1,17 @@
 #ifndef GERENTE_HPP
 #define GERENTE_HPP
 
-#include <iostream>
-#include <iomanip>
-
-using namespace std;
+#include "Funcionario.hpp"
 
 double ValorBONIFICACAO = 15.0;
 
-class Gerente {
-    public:
-        double SalarioBase; // valor mínimo recebido pelo funcionário
-        string IDADE;
-        string nome;
-        int rgFunc;
-        double bonificacao;
-
-
-        void print() {
-            cout << "[Funcionario]" << endl
-            << "[Gerente]" << endl
-            << "  Nome: " << nome << endl
-            << "  Idade: " << IDADE << endl
-            << "  RGFunc: " << rgFunc << endl
-            << "  SalarioBase: R$ " << fixed << setprecision(2) << SalarioBase <<endl;
-        }
-
-        double calcula_BONIFICACAO_GERENTE(int numTOTALVendas){
-            double x;
-            return numTOTALVendas*ValorBONIFICACAO;
-        }
-
+class Gerente : public Funcionario {
+private:
+    double bonificacao;
+public:
+    Gerente(std::string& nome, int idade, int rgFuncionario, double salarioBase, double bonificacao);
+    void imprimeDadosGerente();
+    void calcularBonificacaoGerente(int numTotalVendas);
 };
 
 #endif
