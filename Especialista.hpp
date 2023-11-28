dacd1bd392146d6320c9c818682a6525defe98d6
Atgot <152310870+Atgot@users.noreply.github.com>
2023-11-28 16:12:37 -0300

    Update Especialista.hpp

--- Especialista.hpp
+++ Especialista.hpp
@@ -6,35 +6,35 @@
 
 using namespace std;
 
-double perc = 0.1;
-double percWanda = 0.1;
-
-
 class Especialista : public Funcionario {
     private:
         string _especialidade;
+        int _numAtendimentos;
+        double _comissao;
     public:
         Especialista(string nome, string IDADE, int rgFunc, double SalarioBase, string especialidade):
-        Funcionario(nome, IDADE, rgFunc, SalarioBase), _especialidade(especialidade) {}
+        Funcionario(nome, IDADE, rgFunc, SalarioBase), _especialidade(especialidade), _numAtendimentos=0, _comissao=0 
+        {}
 
-    double comissao(double ValorVenda) {
-        double c = ValorVenda*perc;
-                  return c;
+    void comissao(double ValorVenda) {
+        _numAtendimentos++;
+        double c = ValorVenda*0.1;
+        _comissao+=c;
+    }
+    int numAtendimentos(){
+        return _numAtendimentos;
     }
     void print_nome() {
         cout << nome << endl;
     }
 
     void print() {
-
         cout << "[Especialista]" << endl;
         Funcionario::print();
-
-
-
         cout << "  Nome: " << nome << endl
-        << "  SalarioBase: R$ " << fixed << setprecision(2) << SalarioBase << endl;
-
+        << "  SalarioBase: R$ " << fixed << setprecision(2) << SalarioBase << endl
+        << "Num Atendimentos: " << _numAtendimentos << endl
+        << "Salario Total: " << _SalarioBase+_comissao << endl;
     }
 };
 
