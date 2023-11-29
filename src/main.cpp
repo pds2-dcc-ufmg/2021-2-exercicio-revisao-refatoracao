31f949fb6f09bc73ca78cb2b3fda1277b0cb7d78
Chr1s-J <103655149+Chr1s-J@users.noreply.github.com>
2023-11-28 22:18:10 -0300

    Update main.cpp
    Emprego de camelCase

--- src/main.cpp
+++ src/main.cpp
@@ -31,9 +31,9 @@ int main()
     int numAtendimentos01 = 0;
     double comissao01 = 0;
     e01.nome = "Peter Parker";
-    e01.IDADE = "46";
-    e01.rgFunc = 27061975;
-    e01.SalarioBase = 3000;
+    e01.idade = "46";
+    e01.rgFuncionario = 27061975;
+    e01.salarioBase = 3000;
     e01.especialidade = "Fotografia";
 
 
@@ -42,9 +42,9 @@ int main()
     int numAtendimentos02 = 0;
     double comissao02 = 0;
     e02.nome = "Tony Stark";
-    e02.IDADE = "56";
-    e02.rgFunc = 4041965;
-    e02.SalarioBase = 1000;
+    e02.idade = "56";
+    e02.rgFuncionario = 4041965;
+    e02.salarioBase = 1000;
     e02.especialidade = "Consertos de equipamentos eletronicos";
 
 
@@ -52,26 +52,26 @@ int main()
     int numAtendimentos03 = 0;
     double comissao03 = 0;
     e03.nome = "Wanda Maximoff";
-    e03.IDADE = "32";
-    e03.rgFunc = 16021989;
-    e03.SalarioBase = 5000;
+    e03.idade = "32";
+    e03.rgFuncionario = 16021989;
+    e03.salarioBase = 5000;
     e03.especialidade = "Engenharia e Designeeeeer";
 
 
     Gerente g01;
     double bonificacao01 = 0;
     g01.nome = "Nick Fury";
-    g01.IDADE = "72";
-    g01.rgFunc = 21121948;
-    g01.SalarioBase = 10000;
+    g01.idade = "72";
+    g01.rgFuncionario = 21121948;
+    g01.salarioBase = 10000;
 
 
     Venda v01;
     v01.cliente= "J. Jonah Jameson";
     v01.esp = e01;
     v01.descricao = "Fotos do Homem Aranha";
-    v01.VALOR = 100;
-    comissao01 += e01.comissao(v01.VALOR);
+    v01.valor = 100;
+    comissao01 += e01.comissao(v01.valor);
     numAtendimentos01+=1;
 
 
@@ -79,8 +79,8 @@ int main()
     v02.cliente= "Bruce Benner";
     v02.esp = e02;
     v02.descricao = "Troca da tela do telefone";
-    v02.VALOR = 100;
-    comissao02 += e02.comissao(v02.VALOR);
+    v02.valor = 100;
+    comissao02 += e02.comissao(v02.valor);
     numAtendimentos02+=1;
 
 
@@ -88,8 +88,8 @@ int main()
     v03.cliente= "Norman Osborn";
     v03.esp = e01;
     v03.descricao = "Fotos do novo planador";
-    v03.VALOR = 150;
-    comissao01 += e01.comissao(v03.VALOR);
+    v03.valor = 150;
+    comissao01 += e01.comissao(v03.valor);
     numAtendimentos01+=1;
 
 
@@ -97,8 +97,8 @@ int main()
     v04.cliente= "J. Jonah Jameson";
     v04.esp = e02;
     v04.descricao = "Recarga de cartucho";
-    v04.VALOR = 10;
-    comissao02 += e02.comissao(v04.VALOR);
+    v04.valor = 10;
+    comissao02 += e02.comissao(v04.valor);
     numAtendimentos02+=1;
 
 
@@ -106,8 +106,8 @@ int main()
     v05.cliente= "Bruce Benner";
     v05.esp = e03;
     v05.descricao = "Reconstrucao de Predio";
-    v05.VALOR = 10000;
-    comissao03 += e03.comissao(v05.VALOR);
+    v05.valor = 10000;
+    comissao03 += e03.comissao(v05.valor);
     numAtendimentos03+=1;
 
 
@@ -115,13 +115,13 @@ int main()
     v06.cliente= "Steve Rogers";
     v06.esp = e03;
     v06.descricao = "Decoracao de Apartamento no Brooklyn";
-    v06.VALOR = 3000;
-    comissao03 += e03.comissao(v06.VALOR);
+    v06.valor = 3000;
+    comissao03 += e03.comissao(v06.valor);
     numAtendimentos03+=1;
 
 
     v06.descricao = "Decoracao de Apartamento no Brooklyn";
-    v06.VALOR = 3000;
+    v06.valor = 3000;
 
 
 
@@ -129,8 +129,8 @@ int main()
     v07.cliente= "J. Jonah Jameson";
     v07.esp = e03;
     v07.descricao = "Reforma do Clarim Diario";
-    v07.VALOR = 5000;
-    comissao03 += e03.comissao(v07.VALOR);
+    v07.valor = 5000;
+    comissao03 += e03.comissao(v07.valor);
     numAtendimentos03+=1;
 
 
@@ -138,8 +138,8 @@ int main()
     v08.cliente= "Otto Octavius";
     v08.esp = e02;
     v08.descricao = "Formatacao do PC";
-    v08.VALOR = 80;
-    comissao02 += e02.comissao(v08.VALOR);
+    v08.valor = 80;
+    comissao02 += e02.comissao(v08.valor);
     numAtendimentos02+=1;
 
 
@@ -181,22 +181,22 @@ int main()
     cout <<" \n \n           Relatorio dos Funcionarios \n" << endl;
     e01.print();
     cout << "Num Atendimentos: " << numAtendimentos01 << endl;
-    cout << "Salario Total: " << e01.SalarioBase+comissao01<<endl;
+    cout << "Salario Total: " << e01.salarioBase+comissao01<<endl;
 
     e02.print();
     cout << "Num Atendimentos: " << numAtendimentos02 << endl;
-    cout << "Salario Total: " << e02.SalarioBase+comissao02<<endl;
+    cout << "Salario Total: " << e02.salarioBase+comissao02<<endl;
 
     e03.print();
     cout << "Num Atendimentos: " << numAtendimentos03 << endl;
-    cout << "Salario Total: " << e03.SalarioBase+comissao03<<endl;
+    cout << "Salario Total: " << e03.salarioBase+comissao03<<endl;
 
 
     int NUMTOTALservicos = 0;
     NUMTOTALservicos = numAtendimentos01 + numAtendimentos02 + numAtendimentos03;
-    g01.bonificacao = g01.calcula_BONIFICACAO_GERENTE(NUMTOTALservicos);
+    g01.bonificacao = g01.calculaBonificacaoGerente(NUMTOTALservicos);
     g01.print();
-    cout << "Salario Total: " << g01.SalarioBase + g01.bonificacao<<endl;
+    cout << "Salario Total: " << g01.salarioBase + g01.bonificacao<<endl;
 
     return 0;
 }
