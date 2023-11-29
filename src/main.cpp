c7c39282089f3c01d4e1c1db04a5d8bb23c9aa0d
Chr1s-J <103655149+Chr1s-J@users.noreply.github.com>
2023-11-28 21:25:41 -0300

    Update main.cpp
    Uso de construtor de classe Cliente

--- src/main.cpp
+++ src/main.cpp
@@ -9,34 +9,19 @@ using namespace std;
 
 int main()
 {
-    Cliente cliente1;
-    cliente1.NOME = "J. Jonah Jameson";
-    cliente1.endereco ="Nova York";
-	cliente1.Cep = "35690000";
+    Cliente cliente1 ("J. Jonah Jameson", "35690000", "Nova York");
     cliente1.print();
-
-    Cliente cliente2;
-    cliente2.NOME = "Norman Osborn";
-    cliente2.endereco ="Hartlford";
-	cliente2.Cep = "22061955";
+       
+    Cliente cliente2 ("Norman Osborn", "22061955", "Hartlford");
     cliente2.print();
-
-    Cliente cliente3;
-    cliente3.NOME = "Otto Octavius";
-    cliente3.endereco ="Schenectady";
-	cliente3.Cep = "24051953";
+    
+    Cliente cliente3 ("Otto Octavius", "24051953", "Schenectady");
     cliente3.print();
-
-    Cliente cliente4;
-    cliente4.NOME = "Bruce Benner";
-    cliente4.endereco ="Dayton";
-	cliente4.Cep = "22111967";
+    
+    Cliente cliente4 ("Bruce Benner", "22111967", "Dayton");
     cliente4.print();
 
-    Cliente cliente5;
-    cliente5.NOME = "Steve Rogers";
-    cliente5.endereco ="Lower East Side";
-	cliente5.Cep = "13061981";
+    Cliente cliente5 ("Steve Rogers", "13061981", "Lower East Side");
     cliente5.print();
 
 
