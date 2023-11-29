3892a9b06b963325f5477aa2857656d78ebe79b3
Atgot <152310870+Atgot@users.noreply.github.com>
2023-11-28 22:00:39 -0300

    Update Especialista.hpp
    ;

--- Especialista.hpp
+++ Especialista.hpp
@@ -14,7 +14,7 @@ class Especialista : public Funcionario {
     public:
         Especialista(string nome, string IDADE, int rgFunc, double SalarioBase, string especialidade):
         Funcionario(nome, IDADE, rgFunc, SalarioBase), _especialidade(especialidade), _numAtendimentos(0), _comissao(0) 
-        {}
+        {};
 
     void comissao(double ValorVenda) {
         _numAtendimentos++;
@@ -36,29 +36,5 @@ class Especialista : public Funcionario {
         << "Num Atendimentos: " << _numAtendimentos << endl
         << "Salario Total: " << _SalarioBase+_comissao << endl;
     }
-};
-
-class Gerente: public Funcionario {
-    private:
-        double _bonificacao;
-        double ValorBONIFICACAO = 15.0;
-    public:
-        Gerente(string nome, string IDADE, int rgFunc, double SalarioBase):
-        Funcionario(nome, IDADE, rgFunc, SalarioBase), _bonificacao(0) {}
-        void print() {
-            cout << "[Funcionario]" << endl
-            << "[Gerente]" << endl
-            << "  Nome: " << _nome << endl
-            << "  Idade: " << _IDADE << endl
-            << "  RGFunc: " << _rgFunc << endl
-            << "  SalarioBase: R$ " << fixed << setprecision(2) << _SalarioBase << endl
-            << "Salario Total: " << _SalarioBase + _bonificacao << endl;
-        }
-
-        double calcula_BONIFICACAO_GERENTE(int numTOTALVendas){
-            double _bonificacao=numTOTALVendas*ValorBONIFICACAO;
-            return _bonificacao;
-        }
-
 };
 #endif
