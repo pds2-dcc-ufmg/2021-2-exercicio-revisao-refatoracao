0874d78a0fd9e9105259c1d6c5e1b182ddbfc4b7
Chr1s-J <103655149+Chr1s-J@users.noreply.github.com>
2023-11-28 22:12:45 -0300

    Update Gerente.cpp
    Emprego de camelCase

--- src/Gerente.cpp
+++ src/Gerente.cpp
@@ -6,13 +6,13 @@ void Gerente::print() {
 	std::cout << "[Funcionario]" << std::endl
         << "[Gerente]" << std::endl
         << "  Nome: " << nome << std::endl
-        << "  Idade: " << IDADE << std::endl
-        << "  RGFunc: " << rgFunc << std::endl
-        << "  SalarioBase: R$ " << std::fixed << std::setprecision(2) << SalarioBase << std::endl;
+        << "  Idade: " << idade << std::endl
+        << "  RGFunc: " << rgFuncionario << std::endl
+        << "  SalarioBase: R$ " << std::fixed << std::setprecision(2) << salarioBase << std::endl;
 }
 
-double ValorBONIFICACAO = 15.0;
+double ValorBonificacao = 15.0;
 
-double Gerente::calcula_BONIFICACAO_GERENTE(int numTOTALVendas){	
-        return numTOTALVendas * ValorBONIFICACAO;
+double Gerente::calculaBonificacaoGerente(int numTotalVendas){	
+        return numTotalVendas * ValorBonificacao;
 }
